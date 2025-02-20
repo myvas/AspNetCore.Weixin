0c655ad592cff423730a003ae505afbb28019393
FrankH <4848285@qq.com>
2025-02-20 11:27:17 +0800

    Create an all-in-one project to include other projects

--- src/WeixinCore/Resources.Designer.cs
+++ src/WeixinCore/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Myvas.AspNetCore.Weixin {
+namespace Myvas.AspNetCore.Weixin.Core {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Myvas.AspNetCore.Weixin {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Myvas.AspNetCore.Weixin.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Myvas.AspNetCore.Weixin.Core.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- src/WeixinEfCore/Resources.Designer.cs
+++ src/WeixinEfCore/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Myvas.AspNetCore.Weixin {
+namespace Myvas.AspNetCore.Weixin.EfCore {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Myvas.AspNetCore.Weixin {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Myvas.AspNetCore.Weixin.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Myvas.AspNetCore.Weixin.EfCore.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
