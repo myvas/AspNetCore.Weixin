a918721918779b21c640dc7cf5348a7f2b6d3778
FrankH <4848285@qq.com>
2025-03-05 13:16:33 +0800

    Use TryAdd instead of Add to avoid exception if key already exists

--- test/WeixinApi.Test/TestServers/FakeServerBuilder.cs
+++ test/WeixinApi.Test/TestServers/FakeServerBuilder.cs
@@ -20,14 +20,14 @@ namespace Myvas.AspNetCore.Weixin.Api.Test
                     var appId = req.Query["appid"];
                     if (appId != "APPID")
                     {
-                        context.Response.Headers.Add(HeaderNames.ContentType, "application/json");
+                        context.Response.Headers.TryAdd(HeaderNames.ContentType, "application/json");
                         var content = TestFile.ReadTestJsonFile("AccessToken/invalid_appid.json");
                         await context.Response.WriteAsync(content);
                         return true;
                     }
                     else
                     {
-                        context.Response.Headers.Add(HeaderNames.ContentType, "application/json");
+                        context.Response.Headers.TryAdd(HeaderNames.ContentType, "application/json");
                         var content = TestFile.ReadTestJsonFile("AccessToken/ok.json");
                         await context.Response.WriteAsync(content);
                         return true;
@@ -35,42 +35,42 @@ namespace Myvas.AspNetCore.Weixin.Api.Test
                 }
                 else if (req.Path.Value.EndsWith("/cgi-bin/getcallbackip"))
                 {
-                    context.Response.Headers.Add(HeaderNames.ContentType, "application/json");
+                    context.Response.Headers.TryAdd(HeaderNames.ContentType, "application/json");
                     var content = TestFile.ReadTestJsonFile("Common/getcallbackip.json");
                     await context.Response.WriteAsync(content);
                     return true;
                 }
                 else if (req.Path.Value.EndsWith("/cgi-bin/get_api_domain_ip"))
                 {
-                    context.Response.Headers.Add(HeaderNames.ContentType, "application/json");
+                    context.Response.Headers.TryAdd(HeaderNames.ContentType, "application/json");
                     var content = TestFile.ReadTestJsonFile("Common/get_api_domain_ip.json");
                     await context.Response.WriteAsync(content);
                     return true;
                 }
                 else if (req.Path.Value.EndsWith("/cgi-bin/callback/check"))
                 {
-                    context.Response.Headers.Add(HeaderNames.ContentType, "application/json");
+                    context.Response.Headers.TryAdd(HeaderNames.ContentType, "application/json");
                     var content = TestFile.ReadTestJsonFile("Common/callback_check.json");
                     await context.Response.WriteAsync(content);
                     return true;
                 }
                 else if (req.Path.Value.EndsWith("/cgi-bin/menu/create"))
                 {
-                    context.Response.Headers.Add(HeaderNames.ContentType, "application/json");
+                    context.Response.Headers.TryAdd(HeaderNames.ContentType, "application/json");
                     var content = TestFile.ReadTestJsonFile("Menu/menu_create.json");
                     await context.Response.WriteAsync(content);
                     return true;
                 }
                 else if (req.Path.Value.EndsWith("/cgi-bin/get_current_selfmenu_info"))
                 {
-                    context.Response.Headers.Add(HeaderNames.ContentType, "application/json");
+                    context.Response.Headers.TryAdd(HeaderNames.ContentType, "application/json");
                     var content = TestFile.ReadTestJsonFile("Menu/get_current_selfmenu_info.api.json");
                     await context.Response.WriteAsync(content);
                     return true;
                 }
                 else if (req.Path.Value.EndsWith("/cgi-bin/menu/delete"))
                 {
-                    context.Response.Headers.Add(HeaderNames.ContentType, "application/json");
+                    context.Response.Headers.TryAdd(HeaderNames.ContentType, "application/json");
                     var content = TestFile.ReadTestJsonFile("Menu/menu_delete.json");
                     await context.Response.WriteAsync(content);
                     return true;
