5af2f10387f956e3572ec930877401c76f9a6dc6
FrankH <4848285@qq.com>
2025-03-06 03:06:13 +0800

    Typo in comments

--- test/CommonTest/RealWeixinServiceAccountTestBase.cs
+++ test/CommonTest/RealWeixinServiceAccountTestBase.cs
@@ -22,7 +22,7 @@ namespace Weixin.Tests
         public void TestWithEnvironmentVariable()
         {
             // 如果在您的机器上测试，请在微信公众平台(https://mp.weixin.qq.com)上获取配置，并设置User Secrets或环境变量。
-            // 在本地机器上测试，推荐使用环境变量，键名分别为：WEIXIN_APPID="wx02056e2b2b9cc4ef" dotnet test
+            // 在本地机器上测试，推荐使用环境变量，键名分别为：
             // WEIXIN__APPID
             // WEIXIN__APPSECRET
             // Notes: There are double underscores to replace the colon in the configuration key.
