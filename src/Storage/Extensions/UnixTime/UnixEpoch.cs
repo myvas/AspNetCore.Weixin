e552c5310e2d34d675414e9c3139cdf20febe6c4
FrankH <4848285@qq.com>
2025-03-06 16:29:52 +0800

    UnixEpoch: Add support to netstandard2.0 and below

--- src/Storage/Extensions/UnixTime/UnixEpoch.cs
+++ src/Storage/Extensions/UnixTime/UnixEpoch.cs
@@ -4,21 +4,7 @@
 public static class UnixEpoch
 {
 
-#if NETSTANDARD2_0
-
-    /// <summary>
-    /// Unix epoch to DateTime
-    /// </summary>
-    /// <returns></returns>
-    public static readonly DateTime DateTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-
-    /// <summary>
-    /// Unix epoch to DateTimeOffset
-    /// </summary>
-    /// <returns></returns>
-    public static readonly DateTimeOffset DateTimeOffset = new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero);
-
-#else // NETSTANDARD2_1
+#if NETSTANDARD2_1_OR_GREATER
 
     /// <summary>
     /// Unix epoch to DateTime
@@ -31,6 +17,20 @@ public static class UnixEpoch
     /// </summary>
     /// <returns></returns>
     public static readonly DateTimeOffset DateTimeOffset = DateTimeOffset.UnixEpoch;
+
+#else
+
+    /// <summary>
+    /// Unix epoch to DateTime
+    /// </summary>
+    /// <returns></returns>
+    public static readonly DateTime DateTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+    /// <summary>
+    /// Unix epoch to DateTimeOffset
+    /// </summary>
+    /// <returns></returns>
+    public static readonly DateTimeOffset DateTimeOffset = new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero);
 
 #endif
 
