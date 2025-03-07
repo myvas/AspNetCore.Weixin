205dc11db23d60001cf0579b989ee596f8e64ce9
FrankH <4848285@qq.com>
2025-03-07 13:27:35 +0800

    Add EfCore project from master

---
+++ src/EfCore/WeixinStoreBuilder.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+
+namespace Myvas.AspNetCore.Weixin
+{
+
+	/// <summary>
+	/// Helper functions for configuring Weixin services.
+	/// </summary>
+	public class WeixinStoreBuilder
+	{
+		public WeixinStoreBuilder(Type subscriberType, IServiceCollection services)
+		{
+			SubscriberType = subscriberType;
+			Services = services;
+		}
+
+		public IServiceCollection Services { get; private set; }
+
+        /// <summary>
+        /// Gets the <see cref="Type"/> used for Weixin subscriber.
+        /// </summary>
+        /// <value>
+        /// The <see cref="Type"/> used for Weixin subscriber.
+        /// </value>
+        public Type SubscriberType { get; private set; }
+    }
+}
