8124f9917093a145f373ffaa1495437426b8f032
FrankH <4848285@qq.com>
2025-03-07 13:34:32 +0800

    Add WeixinSiteHost project from 6.0

---
+++ hosts/WeixinSiteHost/Controllers/LocalApiController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Myvas.AspNetCore.WeixinSiteHost;
+
+[Route("localApi")]
+public class LocalApiController : ControllerBase
+{
+    public IActionResult Get()
+    {
+        var claims = from c in User.Claims select new { c.Type, c.Value };
+        return new JsonResult(claims);
+    }
+}
+
