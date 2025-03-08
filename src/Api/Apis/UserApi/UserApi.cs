0a66ce849b9bbe114ee8e278cbf786df6644dd01
FrankH <4848285@qq.com>
2025-03-09 05:14:12 +0800

    Update UserApi

--- src/Api/Apis/UserApi/UserApi.cs
+++ src/Api/Apis/UserApi/UserApi.cs
@@ -1,107 +1,111 @@
 ﻿using Microsoft.Extensions.Options;
 using System.Collections.Generic;
-using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace Myvas.AspNetCore.Weixin
+namespace Myvas.AspNetCore.Weixin;
+
+/// <summary>
+/// 用户接口
+/// <remarks>接口详见：http://mp.weixin.qq.com/wiki/index.php?title=%E8%8E%B7%E5%8F%96%E7%94%A8%E6%88%B7%E5%9F%BA%E6%9C%AC%E4%BF%A1%E6%81%AF</remarks>
+/// </summary>
+public class UserApi : SecureWeixinApiClient, IUserApi
 {
-    /// <summary>
-    /// 用户接口
-    /// <remarks>接口详见：http://mp.weixin.qq.com/wiki/index.php?title=%E8%8E%B7%E5%8F%96%E7%94%A8%E6%88%B7%E5%9F%BA%E6%9C%AC%E4%BF%A1%E6%81%AF</remarks>
-    /// </summary>
-    public class UserApi: SecureApiClient
+    public UserApi(IOptions<WeixinOptions> optionsAccessor, IWeixinAccessTokenApi tokenProvider) : base(optionsAccessor, tokenProvider)
     {
-        public UserApi(IOptions<WeixinApiOptions> optionsAccessor, IWeixinAccessToken tokenProvider) : base(optionsAccessor, tokenProvider)
+    }
+
+    /// <summary>
+    /// 获取微信用户资料
+    /// </summary>
+    /// <param name="accessToken">微信访问凭证</param>
+    /// <param name="openId">微信用户标识。在同一公众号中，一个微信号（用户）每次订阅后得到的OpenId都是唯一的。</param>
+    /// <param name="lang"><see cref="WeixinLanguage"/></param>
+    /// <returns>微信用户资料</returns>
+    /// <remarks>
+    /// 错误时微信会返回错误码等信息，JSON数据包示例如下（该示例为AppID无效错误）:
+    /// <code>
+    /// {"errcode":40013,"errmsg":"invalid appid"}
+    /// </code>
+    /// </remarks>
+    public async Task<UserInfoJson> Info(string openId, WeixinLanguage lang = WeixinLanguage.zh_CN, CancellationToken cancellationToken = default)
+    {
+        var accessToken = await GetTokenAsync(cancellationToken);
+
+        var pathAndQuery = "/cgi-bin/user/info?access_token={0}&openid={1}&lang={2}";
+        var url = Options?.BuildWeixinApiUrl(pathAndQuery);
+        url = string.Format(url, accessToken, openId, lang.ToString());
+
+        return await GetFromJsonAsync<UserInfoJson>(url);
+    }
+
+    /// <summary>
+    /// 获取一个批次的订阅者的OpenId
+    /// </summary>
+    /// <param name="accessToken"></param>
+    /// <param name="nextOpenId"></param>
+    /// <returns></returns>
+    public async Task<UserGetJson> Get(string nextOpenId, CancellationToken cancellationToken = default)
+    {
+        var accessToken = await GetTokenAsync(cancellationToken);
+
+        var pathAndQuery = "/cgi-bin/user/get?access_token={0}";
+        var url = Options?.BuildWeixinApiUrl(pathAndQuery);
+        url = string.Format(url, accessToken);
+        if (!string.IsNullOrEmpty(nextOpenId))
         {
+            url += "&next_openid=" + nextOpenId;
         }
+        return await GetFromJsonAsync<UserGetJson>(url);
+    }
 
-        /// <summary>
-        /// 获取微信用户资料
-        /// </summary>
-        /// <param name="accessToken">微信访问凭证</param>
-        /// <param name="openId">微信用户标识。在同一公众号中，一个微信号（用户）每次订阅后得到的OpenId都是唯一的。</param>
-        /// <param name="lang"><see cref="WeixinLanguage"/></param>
-        /// <returns>微信用户资料</returns>
-        /// <remarks>
-        /// 错误时微信会返回错误码等信息，JSON数据包示例如下（该示例为AppID无效错误）:
-        /// <code>
-        /// {"errcode":40013,"errmsg":"invalid appid"}
-        /// </code>
-        /// </remarks>
-        public async Task< UserInfoJson> Info(string accessToken, string openId, WeixinLanguage lang = WeixinLanguage.zh_CN)
+    /// <summary>
+    /// 获取所有订阅者的OpenId
+    /// </summary>
+    /// <param name="accessToken"></param>
+    /// <returns></returns>
+    public async Task<List<string>> GetAllOpenIds(CancellationToken cancellationToken = default)
+    {
+        List<string> openids = new List<string>();
+
+        string nextOpenId = "";
+        do
         {
-            string url = string.Format(
-                "https://api.weixin.qq.com/cgi-bin/user/info?access_token={0}&openid={1}&lang={2}",
-                accessToken, openId, lang.ToString());
-
-            return await GetFromJsonAsync<UserInfoJson>(url);
-        }
-
-        /// <summary>
-        /// 获取一个批次的订阅者的OpenId
-        /// </summary>
-        /// <param name="accessToken"></param>
-        /// <param name="nextOpenId"></param>
-        /// <returns></returns>
-        public async Task<UserGetJson> Get(string accessToken, string nextOpenId)
-        {
-            string url = string.Format("https://api.weixin.qq.com/cgi-bin/user/get?access_token={0}",
-                accessToken);
-            if (!string.IsNullOrEmpty(nextOpenId))
+            UserGetJson followerResult = await Get(nextOpenId);
+            int count = followerResult.count;
+            if (count > 0)
             {
-                url += "&next_openid=" + nextOpenId;
+                openids.AddRange(followerResult.data.openid);
             }
-            return await GetFromJsonAsync<UserGetJson>(url);
-        }
+            nextOpenId = followerResult.next_openid;
+        } while (!string.IsNullOrEmpty(nextOpenId));
 
-        /// <summary>
-        /// 获取所有订阅者的OpenId
-        /// </summary>
-        /// <param name="accessToken"></param>
-        /// <returns></returns>
-        public async Task<List<string>> GetAllOpenIds(string accessToken)
+        return openids;
+    }
+
+    /// <summary>
+    /// 获取所有订阅者的资料
+    /// </summary>
+    /// <param name="accessToken"></param>
+    /// <returns></returns>
+    public async Task<List<UserInfoJson>> GetAllUserInfo(CancellationToken cancellationToken = default)
+    {
+        List<UserInfoJson> subscribers = new List<UserInfoJson>();
+
+        List<string> openids = await GetAllOpenIds();
+        foreach (string openid in openids)
         {
-            List<string> openids = new List<string>();
-
-            string nextOpenId = "";
-            do
+            try
             {
-                UserGetJson followerResult = await Get(accessToken, nextOpenId);
-                int count = followerResult.count;
-                if (count > 0)
+                UserInfoJson userInfo = await Info(openid);
+                if (userInfo != null)
                 {
-                    openids.AddRange(followerResult.data.openid);
+                    subscribers.Add(userInfo);
                 }
-                nextOpenId = followerResult.next_openid;
-            } while (!string.IsNullOrEmpty(nextOpenId));
-
-            return openids;
-        }
-
-        /// <summary>
-        /// 获取所有订阅者的资料
-        /// </summary>
-        /// <param name="accessToken"></param>
-        /// <returns></returns>
-        public async Task<List<UserInfoJson>> GetAllUserInfo(string accessToken)
-        {
-            List<UserInfoJson> subscribers = new List<UserInfoJson>();
-
-            List<string> openids = await GetAllOpenIds(accessToken);
-            foreach (string openid in openids)
-            {
-                try
-                {
-                    UserInfoJson userInfo = await Info(accessToken, openid);
-                    if (userInfo != null)
-                    {
-                        subscribers.Add(userInfo);
-                    }
-                }
-                catch { }
             }
-
-            return subscribers;
+            catch { }
         }
+
+        return subscribers;
     }
 }
