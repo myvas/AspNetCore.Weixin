bbe0109e0a0f91b25e751d47be9a8a4c71d4fbf0
FrankH <4848285@qq.com>
2025-03-09 05:17:03 +0800

    Update UserGroupApi

--- src/Api/Apis/UserGroupApi/Models/GroupsJson.cs
+++ src/Api/Apis/UserGroupApi/Models/GroupsJson.cs
@@ -1,19 +1,18 @@
 ﻿using System.Collections.Generic;
 
-namespace Myvas.AspNetCore.Weixin
-{
-    public class GroupsJson : WeixinErrorJson
-    {
-        public List<GroupsJson_Group> groups { get; set; }
-    }
+namespace Myvas.AspNetCore.Weixin;
 
-    public class GroupsJson_Group
-    {
-        public int id { get; set; }
-        public string name { get; set; }
-        /// <summary>
-        /// 此属性在CreateGroupResult的Json数据中，创建结果中始终为0
-        /// </summary>
-        public int count { get; set; }
-    }
+public class GroupsJson : WeixinErrorJson
+{
+    public List<GroupsJson_Group> groups { get; set; }
+}
+
+public class GroupsJson_Group
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    /// <summary>
+    /// 此属性在CreateGroupResult的Json数据中，创建结果中始终为0
+    /// </summary>
+    public int count { get; set; }
 }
