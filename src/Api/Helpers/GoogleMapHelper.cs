28701e7b8fe3736446bbe8eecbd154af5b89d3c3
FrankH <4848285@qq.com>
2025-03-09 05:18:56 +0800

    Update Helpers

--- src/Api/Helpers/GoogleMapHelper.cs
+++ src/Api/Helpers/GoogleMapHelper.cs
@@ -1,42 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Text;
 
-namespace Myvas.AspNetCore.Weixin
+namespace Myvas.AspNetCore.Weixin;
+
+public static class GoogleMapHelper
 {
-    public static class GoogleMapHelper
+    /// <summary>
+    /// 获取谷歌今天静态地图Url。API介绍：https://developers.google.com/maps/documentation/staticmaps/?hl=zh-CN
+    /// </summary>
+    /// <returns></returns>
+    public static string GetGoogleStaticMap(int scale,  IList<GoogleMapMarkers> markersList, string size = "640x640")
     {
-        /// <summary>
-        /// 获取谷歌今天静态地图Url。API介绍：https://developers.google.com/maps/documentation/staticmaps/?hl=zh-CN
-        /// </summary>
-        /// <returns></returns>
-        public static string GetGoogleStaticMap(int scale,  IList<GoogleMapMarkers> markersList, string size = "640x640")
+        markersList = markersList ?? new List<GoogleMapMarkers>();
+        StringBuilder markersStr = new StringBuilder();
+        foreach (var markers in markersList)
         {
-            markersList = markersList ?? new List<GoogleMapMarkers>();
-            StringBuilder markersStr = new StringBuilder();
-            foreach (var markers in markersList)
+            markersStr.Append("&markers=");
+            if (markers.Size != GoogleMapMarkerSize.mid)
             {
-                markersStr.Append("&markers=");
-                if (markers.Size != GoogleMapMarkerSize.mid)
-                {
-                    markersStr.AppendFormat("size={0}%7C", markers.Size);
-                }
-                if (!string.IsNullOrEmpty(markers.Color))
-                {
-                    markersStr.AppendFormat("color:{0}%7C", markers.Color);
-                }
-                markersStr.Append("label:");
-                if (!string.IsNullOrEmpty(markers.Label))
-                {
-                    markersStr.AppendFormat("{0}%7C", markers.Label);
-                }
-                markersStr.AppendFormat("{0},{1}", markers.Latitude, markers.Longitude);
+                markersStr.AppendFormat("size={0}%7C", markers.Size);
             }
-            string parameters = string.Format("center=&zoom=&size={0}&maptype=roadmap&format=jpg&sensor=false&language=zh&{1}", 
-                                             size,markersStr.ToString());
-            string url = "http://maps.googleapis.com/maps/api/staticmap?" + parameters;
-            return url;
+            if (!string.IsNullOrEmpty(markers.Color))
+            {
+                markersStr.AppendFormat("color:{0}%7C", markers.Color);
+            }
+            markersStr.Append("label:");
+            if (!string.IsNullOrEmpty(markers.Label))
+            {
+                markersStr.AppendFormat("{0}%7C", markers.Label);
+            }
+            markersStr.AppendFormat("{0},{1}", markers.Latitude, markers.Longitude);
         }
+        string parameters = string.Format("center=&zoom=&size={0}&maptype=roadmap&format=jpg&sensor=false&language=zh&{1}", 
+                                         size,markersStr.ToString());
+        string url = "http://maps.googleapis.com/maps/api/staticmap?" + parameters;
+        return url;
     }
 }
