75262afef1ea234a1455432e89487172559230a8
FrankH <4848285@qq.com>
2025-03-10 22:18:53 +0800

    Add UnixTime

---
+++ src/Api/Abstrations/Extensions/UnixTime/UnixDateTime.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) 2022 FrankH (4848285@qq.com)
+
+namespace System;
+
+/// <summary>
+/// The <see cref="DateTime"/> wrapper for Unix time seconds.
+/// </summary>
+public readonly struct UnixDateTime
+{
+    /// <summary>
+    /// The value.
+    /// </summary>
+    public DateTime DateTime { get; }
+
+    /// <summary>
+    /// The basic functional constructor.
+    /// </summary>
+    /// <param name="dateTime">The <see cref="DateTime"/>.</param>
+    public UnixDateTime(DateTime dateTime)
+    {
+        DateTime = dateTime;
+    }
+
+    /// <summary>
+    /// The default constructor.
+    /// </summary>
+    public UnixDateTime() : this(new DateTime())
+    {
+    }
+
+    /// <summary>
+    /// The copy constructor.
+    /// </summary>
+    /// <param name="unixTimeSeconds">The Unix time seconds.</param>
+    public UnixDateTime(long unixTimeSeconds) : this(UnixEpoch.DateTime.AddSeconds(unixTimeSeconds))
+    {
+    }
+
+    /// <summary>
+    /// Implicit conversion operator from <see cref="DateTime"/>.
+    /// </summary>
+    /// <param name="value">The <see cref="DateTime"/>.</param>
+    public static implicit operator UnixDateTime(DateTime value)
+    {
+        return new UnixDateTime(value);
+    }
+
+    /// <summary>
+    /// Implicit conversion operator to <see cref="DateTime"/>.
+    /// </summary>
+    /// <param name="value">The <see cref="UnixDateTime"/>.</param>
+    public static implicit operator DateTime(UnixDateTime value)
+    {
+        return value.DateTime;
+    }
+}
---
+++ src/Api/Abstrations/Extensions/UnixTime/UnixDateTimeOffset.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) 2022 FrankH (4848285@qq.com)
+
+namespace System;
+
+/// <summary>
+/// The <see cref="System.DateTimeOffset"/> wapper for Unix time seconds.
+/// </summary>
+public readonly struct UnixDateTimeOffset
+{
+    /// <summary>
+    /// The value.
+    /// </summary>
+    public DateTimeOffset DateTimeOffset { get; }
+
+    /// <summary>
+    /// The basic functional constructor.
+    /// </summary>
+    /// <param name="value">The <see cref="DateTime"/>.</param>
+    public UnixDateTimeOffset(DateTimeOffset value)
+    {
+        DateTimeOffset = value;
+    }
+
+    /// <summary>
+    /// The default constructor.
+    /// </summary>
+    public UnixDateTimeOffset() : this(new DateTimeOffset())
+    {
+    }
+
+    /// <summary>
+    /// The copy constructor.
+    /// </summary>
+    /// <param name="unixTimeSeconds">The Unix time seconds.</param>
+    public UnixDateTimeOffset(long unixTimeSeconds) : this(UnixEpoch.DateTimeOffset.AddSeconds(unixTimeSeconds))
+    {
+    }
+
+    /// <summary>
+    /// Implicit conversion operator from <see cref="DateTimeOffset"/>.
+    /// </summary>
+    /// <param name="value">The <see cref="System.DateTimeOffset"/>.</param>
+    public static implicit operator UnixDateTimeOffset(DateTimeOffset value)
+    {
+        return new UnixDateTimeOffset(value);
+    }
+
+    /// <summary>
+    /// Implicit conversion operator to <see cref="DateTimeOffset"/>.
+    /// </summary>
+    /// <param name="value">The <see cref="UnixDateTimeOffset"/>.</param>
+    public static implicit operator DateTimeOffset(UnixDateTimeOffset value)
+    {
+        return value.DateTimeOffset;
+    }
+}
