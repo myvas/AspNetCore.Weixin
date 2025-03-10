179c5bac90388b36fe1d9f64fdb12f7d5706e427
FrankH <4848285@qq.com>
2025-03-11 03:31:02 +0800

    Convert to file-scoped namespace

--- src/Site/Signature/BrowserDetector.cs
+++ src/Site/Signature/BrowserDetector.cs
@@ -1,55 +1,48 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Diagnostics.Tracing;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Net.Http;
+﻿using System.Linq;
 using Microsoft.AspNetCore.Http;
 using System;
 using System.Diagnostics;
 
-namespace Myvas.AspNetCore.Weixin
+namespace Myvas.AspNetCore.Weixin;
+
+public static class BrowserDetector
 {
-    public static class BrowserDetector
+    public static (bool IsMicroMessenger, string Version) DetectMicroMessenger(this HttpContext context)
     {
-        public static (bool IsMicroMessenger, string Version) DetectMicroMessenger(this HttpContext context)
+        var key = "MicroMessenger";
+
+        bool isMicroMessenger = false;
+        string version = "";
+        try
         {
-            var key = "MicroMessenger";
-
-            bool isMicroMessenger = false;
-            string version = "";
-            try
+            var userAgentHeader = context.Request.Headers?.FirstOrDefault(x => x.Key.ToLower() == "User-Agent".ToLower()).Value;
+            if (userAgentHeader.HasValue)
             {
-                var userAgentHeader = context.Request.Headers?.FirstOrDefault(x => x.Key.ToLower() == "User-Agent".ToLower()).Value;
-                if (userAgentHeader.HasValue)
+                var userAgent = userAgentHeader.ToString().AsSpan();
+                var microMessengerIndex = userAgent.IndexOf((key + "/").AsSpan());
+                if (microMessengerIndex > -1)
                 {
-                    var userAgent = userAgentHeader.ToString().AsSpan();
-                    var microMessengerIndex = userAgent.IndexOf((key + "/").AsSpan());
-                    if (microMessengerIndex > -1)
-                    {
-                        isMicroMessenger = true;
+                    isMicroMessenger = true;
 
-                        var versionIndexStart = microMessengerIndex + key.Length + 1;
-                        var versionSlice = userAgent.Slice(versionIndexStart);
-                        var versionIndexEnd = versionSlice.IndexOf(' ');
-                        if (versionIndexEnd > -1)
-                        {
-                            version = versionSlice.Slice(0, versionIndexEnd).ToString();
-                        }
-                        else
-                        {
-                            version = versionSlice.ToString();
-                        }
+                    var versionIndexStart = microMessengerIndex + key.Length + 1;
+                    var versionSlice = userAgent.Slice(versionIndexStart);
+                    var versionIndexEnd = versionSlice.IndexOf(' ');
+                    if (versionIndexEnd > -1)
+                    {
+                        version = versionSlice.Slice(0, versionIndexEnd).ToString();
+                    }
+                    else
+                    {
+                        version = versionSlice.ToString();
                     }
                 }
             }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex);
-            }
-
-            return (isMicroMessenger, version);
         }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+        }
+
+        return (isMicroMessenger, version);
     }
 }
