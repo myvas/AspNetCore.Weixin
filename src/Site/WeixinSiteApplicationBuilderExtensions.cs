48d6033ff39a12fc0b4fc2ddea26f1912cd2a062
FrankH <4848285@qq.com>
2025-03-11 18:35:55 +0800

    .

--- src/Site/WeixinSiteApplicationBuilderExtensions.cs
+++ src/Site/WeixinSiteApplicationBuilderExtensions.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace Myvas.AspNetCore.Weixin;
 
@@ -9,15 +12,64 @@ namespace Myvas.AspNetCore.Weixin;
 public static class WeixinSiteApplicationBuilderExtensions
 {
     /// <summary>
-    /// Adds the WeixinWelcomePageMiddleware to the pipeline with the DI options.
+    /// Adds the <see cref="WeixinSiteMiddleware"/> to the pipeline with the given path.
     /// </summary>
+    /// <remarks>Usage:
+    /// <code>
+    /// app.UseWeixinSite(o =>
+    /// {
+    ///    o.Path = "/wx";
+    ///    o.Debug = true;
+    /// });
+    /// </code>
+    /// </remarks>
     /// <param name="app"></param>
+    /// <param name="path">The path.</param>
     /// <returns></returns>
-    public static IApplicationBuilder UseWeixinSite(this IApplicationBuilder app)
+    public static IApplicationBuilder UseWeixinSite(this IApplicationBuilder app, Action<WeixinSiteOptions> setupAction = null)
     {
         if (app == null)
         {
             throw new ArgumentNullException(nameof(app));
+        }
+
+        if (setupAction != null)
+        {
+            var optionsMonitor = app.ApplicationServices.GetRequiredService<IOptionsMonitor<WeixinSiteOptions>>();
+            var options = optionsMonitor.CurrentValue;
+            setupAction(options);
+        }
+
+        return app.UseMiddleware<WeixinSiteMiddleware>();
+    }
+
+    /// <summary>
+    /// Adds the <see cref="WeixinSiteMiddleware"/> to the pipeline with the given path.
+    /// </summary>
+    /// <remarks>Usage:
+    /// <code>
+    /// app.UseWeixinSite("/wx");
+    /// </code>
+    /// </remarks>
+    /// <param name="app"></param>
+    /// <param name="path">The path.</param>
+    /// <returns></returns>
+    public static IApplicationBuilder UseWeixinSite(this IApplicationBuilder app, PathString path)
+    {
+        if (app == null)
+        {
+            throw new ArgumentNullException(nameof(app));
+        }
+
+        if (string.IsNullOrEmpty(path.Value))
+        {
+            throw new ArgumentException("The path must be a non-empty string.", nameof(path));
+        }
+        else
+        {
+            var optionsMonitor = app.ApplicationServices.GetRequiredService<IOptionsMonitor<WeixinSiteOptions>>();
+            var options = optionsMonitor.CurrentValue;
+            options.Path = path.Value;
         }
 
         return app.UseMiddleware<WeixinSiteMiddleware>();
