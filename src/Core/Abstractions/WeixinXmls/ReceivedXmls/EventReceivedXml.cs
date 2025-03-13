f18b7783fdd52ce6642547dc19ca8eb035bfa988
FrankH <4848285@qq.com>
2025-03-13 23:21:06 +0800

    Add remarks to EventReceivedXml and MessageReceivedXml for documentation clarity

--- src/Core/Abstractions/WeixinXmls/ReceivedXmls/EventReceivedXml.cs
+++ src/Core/Abstractions/WeixinXmls/ReceivedXmls/EventReceivedXml.cs
@@ -5,6 +5,7 @@ namespace Myvas.AspNetCore.Weixin;
 /// <summary>
 /// 收到事件（非普通消息）
 /// </summary>
+/// <remarks>https://developers.weixin.qq.com/doc/offiaccount/Message_Management/Receiving_event_pushes.html</remarks>
 [XmlRoot("xml", Namespace = "")]
 public class EventReceivedXml : ReceivedXml
 {
--- src/Core/Abstractions/WeixinXmls/ReceivedXmls/MessageReceivedXml.cs
+++ src/Core/Abstractions/WeixinXmls/ReceivedXmls/MessageReceivedXml.cs
@@ -6,6 +6,7 @@ namespace Myvas.AspNetCore.Weixin;
 /// <summary>
 /// 收到普通消息（除事件外的消息）
 /// </summary>
+/// <remarks>https://developers.weixin.qq.com/doc/offiaccount/Message_Management/Receiving_standard_messages.html</remarks>
 [XmlRoot("xml", Namespace = "")]
 public class MessageReceivedXml : ReceivedXml
 {
