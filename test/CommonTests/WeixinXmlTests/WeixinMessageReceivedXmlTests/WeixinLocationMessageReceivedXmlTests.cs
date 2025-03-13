2770a7b482d48f36a38b53470c9321ce4d73ea81
FrankH <4848285@qq.com>
2025-03-14 01:06:59 +0800

    Add unit tests for Link, Location, ShortVideo, Video, and Voice messages XML serialization and deserialization

---
+++ test/CommonTests/WeixinXmlTests/WeixinMessageReceivedXmlTests/WeixinLocationMessageReceivedXmlTests.cs
@@ -0,0 +1,57 @@
+using Myvas.AspNetCore.Weixin;
+
+namespace Myvas.AspNetCore.Weixin.CommonTests.WeixinXmlTests;
+
+public class WeixinLocationMessageReceivedXmlTests
+{
+    [Fact]
+    public void Serialize()
+    {
+        string s = TestFile.ReadTestFile("msg/msg-location.xml.txt");
+        var o = new LocationMessageReceivedXml
+        {
+            ToUserName = "gh_712b448adf85",
+            FromUserName = "oI3UkuL9uZxTOuj--HHMSMTlO3ks",
+            CreateTime = 1490872329,
+            MsgType = "text",
+            MsgId = 6403247895999455936,
+            Latitude = 23.134521m,
+            Longitude = 113.358803m,
+            Scale = 20,
+            Label = "位置信息"
+        };
+
+        var result = MyvasXmlConvert.SerializeObject(o);
+        var s2 = WeixinXmlStringNormalizer.Normalize(s);
+        Assert.Equal(s2, result);
+    }
+
+    [Fact]
+    public void Deserialize()
+    {
+        string s = TestFile.ReadTestFile("msg/msg-location.xml.txt");
+        var o = new LocationMessageReceivedXml
+        {
+            ToUserName = "gh_712b448adf85",
+            FromUserName = "oI3UkuL9uZxTOuj--HHMSMTlO3ks",
+            CreateTime = 1490872329,
+            MsgType = "text",
+            MsgId = 6403247895999455936,
+            Latitude = 23.134521m,
+            Longitude = 113.358803m,
+            Scale = 20,
+            Label = "位置信息"
+        };
+
+        var result = MyvasXmlConvert.DeserializeObject<LocationMessageReceivedXml>(s);
+        Assert.Equal(o.FromUserName, result.FromUserName);
+        Assert.Equal(o.ToUserName, result.ToUserName);
+        Assert.Equal(o.CreateTime, result.CreateTime);
+        Assert.Equal(o.MsgType, result.MsgType);
+        Assert.Equal(o.MsgId, result.MsgId);
+        Assert.Equal(o.Latitude, result.Latitude);
+        Assert.Equal(o.Longitude, result.Longitude);
+        Assert.Equal(o.Scale, result.Scale);
+        Assert.Equal(o.Label, result.Label);
+    }
+}
---
+++ test/CommonTests/WeixinXmlTests/WeixinMessageReceivedXmlTests/WeixinVoiceMessageReceivedXml_WithRecognitionTests.cs
@@ -0,0 +1,54 @@
+using Myvas.AspNetCore.Weixin;
+
+namespace Myvas.AspNetCore.Weixin.CommonTests.WeixinXmlTests;
+
+public class WeixinVoiceMessageReceivedXml_WithRecognitionTests
+{
+    [Fact]
+    public void Serialize()
+    {
+        string s = TestFile.ReadTestFile("msg/msg-voice-recognition.xml.txt");
+        var o = new VoiceMessageReceivedXml
+        {
+            ToUserName = "gh_712b448adf85",
+            FromUserName = "oI3UkuL9uZxTOuj--HHMSMTlO3ks",
+            CreateTime = 1490872329,
+            MsgType = "text",
+            MsgId = 6403247895999455936,
+            MediaId = "media_id",
+            Format = "Format",
+            Recognition = "腾讯微信团队"
+        };
+
+        var result = MyvasXmlConvert.SerializeObject(o);
+        var s2 = WeixinXmlStringNormalizer.Normalize(s);
+        Assert.Equal(s2, result);
+    }
+
+    [Fact]
+    public void Deserialize()
+    {
+        string s = TestFile.ReadTestFile("msg/msg-voice-recognition.xml.txt");
+        var o = new VoiceMessageReceivedXml
+        {
+            ToUserName = "gh_712b448adf85",
+            FromUserName = "oI3UkuL9uZxTOuj--HHMSMTlO3ks",
+            CreateTime = 1490872329,
+            MsgType = "text",
+            MsgId = 6403247895999455936,
+            MediaId = "media_id",
+            Format = "Format",
+            Recognition = "腾讯微信团队"
+        };
+
+        var result = MyvasXmlConvert.DeserializeObject<VoiceMessageReceivedXml>(s);
+        Assert.Equal(o.FromUserName, result.FromUserName);
+        Assert.Equal(o.ToUserName, result.ToUserName);
+        Assert.Equal(o.CreateTime, result.CreateTime);
+        Assert.Equal(o.MsgType, result.MsgType);
+        Assert.Equal(o.MsgId, result.MsgId);
+        Assert.Equal(o.MediaId, result.MediaId);
+        Assert.Equal(o.Format, result.Format);
+        Assert.Equal(o.Recognition, result.Recognition);
+    }
+}
