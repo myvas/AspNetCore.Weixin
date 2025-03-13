e567fc36068a0166ce0b9f1c48cebcd193740df7
FrankH <4848285@qq.com>
2025-03-13 22:35:40 +0800

    Add XmlRoot attribute to EventReceivedXml class for XML serialization

--- src/Core/Abstractions/WeixinXmls/ReceivedXmls/EventReceivedXml.cs
+++ src/Core/Abstractions/WeixinXmls/ReceivedXmls/EventReceivedXml.cs
@@ -5,6 +5,7 @@ namespace Myvas.AspNetCore.Weixin;
 /// <summary>
 /// 收到事件（非普通消息）
 /// </summary>
+[XmlRoot("xml", Namespace = "")]
 public class EventReceivedXml : ReceivedXml
 {
     /// <summary>
