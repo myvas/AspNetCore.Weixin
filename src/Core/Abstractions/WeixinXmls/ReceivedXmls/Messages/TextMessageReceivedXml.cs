54139702a4d728d27eeca638b0eb1f31431a3eed
FrankH <4848285@qq.com>
2025-03-13 23:59:17 +0800

    Refactor TextMessageReceivedXml for clarity and improve XML documentation for Content property

--- src/Core/Abstractions/WeixinXmls/ReceivedXmls/Messages/TextMessageReceivedXml.cs
+++ src/Core/Abstractions/WeixinXmls/ReceivedXmls/Messages/TextMessageReceivedXml.cs
@@ -3,10 +3,13 @@
 namespace Myvas.AspNetCore.Weixin;
 
 /// <summary>
-/// 收到文本消息
+/// 文本消息
 /// </summary>
 [XmlRoot("xml", Namespace = "")]
 public class TextMessageReceivedXml : MessageReceivedXml
 {
+    /// <summary>
+    /// 文本消息内容
+    /// </summary>
     public string Content { get; set; }
 }
