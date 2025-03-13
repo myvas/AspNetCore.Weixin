8fdf9897675cedb9247a85ddc9551aa1c8219430
FrankH <4848285@qq.com>
2025-03-13 16:17:31 +0800

    Add unit tests for WeixinEventReceivedXml serialization and deserialization

---
+++ test/CommonTests/WeixinXmlTests/InternalTests/WeixinEventReceivedXmlTests.cs
@@ -0,0 +1,66 @@
+using Myvas.AspNetCore.Weixin;
+
+namespace Myvas.AspNetCore.Weixin.CommonTests.WeixinXmlTests.InternalTests;
+
+public class WeixinEventReceivedXmlTests
+{
+    [Fact]
+    public void WeixinEventReceivedXml_Serialize()
+    {
+        string s = @"<xml><ToUserName></ToUserName><FromUserName></FromUserName><CreateTime>0</CreateTime><MsgType></MsgType><Event><Event></xml>";
+        var o = new EventReceivedXml();
+
+        var result = MyvasXmlConvert.SerializeObject(o);
+        Assert.Equal(s, result);
+    }
+
+    [Fact]
+    public void WeixinEventReceivedXml_SerializeSimple()
+    {
+        string s = @"<xml><ToUserName>To</ToUserName><FromUserName>From</FromUserName><CreateTime>1490872329</CreateTime><MsgType>text</MsgType><Event>LOCATION</Event></xml>";
+        var o = new EventReceivedXml
+        {
+            ToUserName = "To",
+            FromUserName = "From",
+            CreateTime = 1490872329,
+            MsgType = "text",
+            Event = "LOCATION"
+        };
+
+        var result = MyvasXmlConvert.SerializeObject(s);
+        Assert.Equal(s, result);
+    }
+
+    [Fact]
+    public void WeixinReceivedXml_Deserialize()
+    {
+        string s = @"<xml><ToUserName></ToUserName><FromUserName></FromUserName><CreateTime>0</CreateTime><MsgType></MsgType><Event></Event></xml>";
+        var o = new EventReceivedXml();
+
+        var result = MyvasXmlConvert.DeserializeObject<EventReceivedXml>(s);
+        Assert.Equal(o.FromUserName, result.FromUserName);
+        Assert.Equal(o.ToUserName, result.ToUserName);
+        Assert.Equal(o.CreateTime, result.CreateTime);
+        Assert.Equal(o.Event, result.Event);
+    }
+
+    [Fact]
+    public void WeixinReceivedXml_DeserializeSimple()
+    {
+        string s = @"<xml><ToUserName>To</ToUserName><FromUserName>From</FromUserName><CreateTime>1490872329</CreateTime><MsgType>text</MsgType><Event>LOCATION</Event></xml>";
+        var o = new EventReceivedXml
+        {
+            ToUserName = "To",
+            FromUserName = "From",
+            CreateTime = 1490872329,
+            MsgType = "text",
+            Event = "LOCATION"
+        };
+
+        var result = MyvasXmlConvert.DeserializeObject<EventReceivedXml>(s);
+        Assert.Equal(o.FromUserName, result.FromUserName);
+        Assert.Equal(o.ToUserName, result.ToUserName);
+        Assert.Equal(o.CreateTime, result.CreateTime);
+        Assert.Equal(o.Event, result.Event);
+    }
+}
