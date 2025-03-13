191986fc1a7016631bd4847c55a0545c5199f0c5
FrankH <4848285@qq.com>
2025-03-13 23:11:31 +0800

    Rename serialization and deserialization test methods for consistency

--- test/CommonTests/WeixinXmlTests/InternalTests/WeixinReceivedXmlTests.cs
+++ test/CommonTests/WeixinXmlTests/InternalTests/WeixinReceivedXmlTests.cs
@@ -5,7 +5,7 @@ namespace Myvas.AspNetCore.Weixin.CommonTests.WeixinXmlTests.InternalTests;
 public class WeixinReceivedXmlTests
 {
     [Fact]
-    public void WeixinReceivedXml_Serialize()
+    public void Serialize()
     {
         string s = @"<xml><ToUserName></ToUserName><FromUserName></FromUserName><CreateTime>0</CreateTime><MsgType></MsgType></xml>";
         var o = new ReceivedXml();
@@ -15,7 +15,7 @@ public class WeixinReceivedXmlTests
     }
 
     [Fact]
-    public void WeixinReceivedXml_SerializeSimple()
+    public void SerializeSimple()
     {
         string s = @"<xml><ToUserName>To</ToUserName><FromUserName>From</FromUserName><CreateTime>1490872329</CreateTime><MsgType>text</MsgType></xml>";
         var o = new ReceivedXml
@@ -31,7 +31,7 @@ public class WeixinReceivedXmlTests
     }
 
     [Fact]
-    public void WeixinReceivedXml_Deserialize()
+    public void Deserialize()
     {
         string s = @"<xml><ToUserName></ToUserName><FromUserName></FromUserName><CreateTime>0</CreateTime><MsgType></MsgType></xml>";
         var o = new ReceivedXml
@@ -50,7 +50,7 @@ public class WeixinReceivedXmlTests
     }
 
     [Fact]
-    public void WeixinReceivedXml_DeserializeSimple()
+    public void DeserializeSimple()
     {
         string s = @"<xml><ToUserName>To</ToUserName><FromUserName>From</FromUserName><CreateTime>1490872329</CreateTime><MsgType>text</MsgType></xml>";
         var o = new ReceivedXml
