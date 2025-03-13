8cf541d28edea78a5c43c74536df3144910fa3e4
FrankH <4848285@qq.com>
2025-03-13 23:58:42 +0800

    Enhance ImageMessageReceivedXml with XML documentation for PicUrl and MediaId properties

--- src/Core/Abstractions/WeixinXmls/ReceivedXmls/Messages/ImageMessageReceivedXml.cs
+++ src/Core/Abstractions/WeixinXmls/ReceivedXmls/Messages/ImageMessageReceivedXml.cs
@@ -8,7 +8,14 @@ namespace Myvas.AspNetCore.Weixin;
 [XmlRoot("xml", Namespace = "")]
 public class ImageMessageReceivedXml : MessageReceivedXml
 {
+    /// <summary>
+    /// 图片链接（由系统生成）
+    /// </summary>
     public string PicUrl { get; set; }
+
+    /// <summary>
+    /// 图片消息媒体id，可以调用获取临时素材接口拉取数据。
+    /// </summary>
     public string MediaId { get; set; }
 
 }
