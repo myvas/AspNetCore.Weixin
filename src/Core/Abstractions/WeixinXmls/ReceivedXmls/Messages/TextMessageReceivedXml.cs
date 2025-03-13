f95b9df1221700d12d2a8d84b8c6cb45811a7fd5
FrankH <4848285@qq.com>
2025-03-13 22:56:45 +0800

    Refactor TextMessageReceivedXml class by adding XML serialization attributes and improving code structure

--- src/Core/Abstractions/WeixinXmls/ReceivedXmls/Messages/TextMessageReceivedXml.cs
+++ src/Core/Abstractions/WeixinXmls/ReceivedXmls/Messages/TextMessageReceivedXml.cs
@@ -1,10 +1,12 @@
-﻿namespace Myvas.AspNetCore.Weixin
+﻿using System.Xml.Serialization;
+
+namespace Myvas.AspNetCore.Weixin;
+
+/// <summary>
+/// 收到文本消息
+/// </summary>
+[XmlRoot("xml", Namespace = "")]
+public class TextMessageReceivedXml : MessageReceivedXml
 {
-    /// <summary>
-    /// 收到文本消息
-    /// </summary>
-    public class TextMessageReceivedXml : MessageReceivedXml
-    {
-        public string Content { get; set; }
-    }
+    public string Content { get; set; }
 }
