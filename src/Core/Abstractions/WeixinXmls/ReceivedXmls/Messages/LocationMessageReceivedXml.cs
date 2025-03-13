ba6260bc8158e1c24593d23a372d19b600135996
FrankH <4848285@qq.com>
2025-03-13 23:58:58 +0800

    Refactor LocationMessageReceivedXml for clarity and improve XML documentation for properties

--- src/Core/Abstractions/WeixinXmls/ReceivedXmls/Messages/LocationMessageReceivedXml.cs
+++ src/Core/Abstractions/WeixinXmls/ReceivedXmls/Messages/LocationMessageReceivedXml.cs
@@ -1,41 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Xml.Serialization;
+﻿using System.Xml.Serialization;
 
-namespace Myvas.AspNetCore.Weixin
+namespace Myvas.AspNetCore.Weixin;
+
+/// <summary>
+/// 收到地理位置消息
+/// </summary>
+[XmlRoot("xml", Namespace = "")]
+public class LocationMessageReceivedXml : MessageReceivedXml
 {
     /// <summary>
-    /// 收到地理位置消息
+    /// Location_X, 纬度
+    /// <example>23.134521</example>
     /// </summary>
-    [XmlRoot("xml", Namespace = "")]
-    public class LocationMessageReceivedXml : MessageReceivedXml
-    {
-        /// <summary>
-        /// Location_X, 纬度
-        /// <example>23.134521</example>
-        /// </summary>
-        [XmlElement("Location_X", Namespace ="")]
-        public decimal Latitude { get; set; }
+    [XmlElement("Location_X", Namespace = "")]
+    public decimal Latitude { get; set; }
 
-        /// <summary>
-        /// Location_Y, 经度
-        /// <example>113.358803</example>
-        /// </summary>
-        [XmlElement("Location_Y", Namespace = "")]
-        public decimal Longitude { get; set; }
+    /// <summary>
+    /// Location_Y, 经度
+    /// <example>113.358803</example>
+    /// </summary>
+    [XmlElement("Location_Y", Namespace = "")]
+    public decimal Longitude { get; set; }
 
-        /// <summary>
-        /// 缩放大小
-        /// <example>20</example>
-        /// <remarks>注意：与Google Maps不一致</remarks>
-        /// </summary>
-        public decimal Scale { get; set; }
+    /// <summary>
+    /// 缩放大小
+    /// <example>20</example>
+    /// <remarks>注意：与Google Maps不一致</remarks>
+    /// </summary>
+    public decimal Scale { get; set; }
 
-        /// <summary>
-        /// 地址标注
-        /// </summary>
-        public string Label { get; set; }
-    }
+    /// <summary>
+    /// 地址标注
+    /// </summary>
+    public string Label { get; set; }
 }
