fa689c67c774feba4b3460146d4485ad3530083c
FrankH <4848285@qq.com>
2025-03-13 16:16:50 +0800

    Refactor MessageReceivedXml class to simplify structure and remove unnecessary using directives

--- src/Core/Abstractions/WeixinXmls/ReceivedXmls/MessageReceivedXml.cs
+++ src/Core/Abstractions/WeixinXmls/ReceivedXmls/MessageReceivedXml.cs
@@ -1,17 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Xml.Serialization;
 
-namespace Myvas.AspNetCore.Weixin
+namespace Myvas.AspNetCore.Weixin;
+
+/// <summary>
+/// 收到普通消息（除事件外的消息）
+/// </summary>
+public class MessageReceivedXml : ReceivedXml
 {
-    /// <summary>
-    /// 收到普通消息（除事件外的消息）
-    /// </summary>
-    [XmlRoot("xml", Namespace = "")]
-    public class MessageReceivedXml : ReceivedXml
-    {
-        public Int64 MsgId { get; set; }
-    }
+    public Int64 MsgId { get; set; }
 }
