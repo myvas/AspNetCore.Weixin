0d0489882ec9648b261eee9e32b0e9df81d635cb
FrankH <4848285@qq.com>
2025-03-13 20:49:59 +0800

    Refactor TextMessageReceivedXml class by removing unnecessary using directives and XML root attribute

--- src/Core/Abstractions/WeixinXmls/ReceivedXmls/Messages/TextMessageReceivedXml.cs
+++ src/Core/Abstractions/WeixinXmls/ReceivedXmls/Messages/TextMessageReceivedXml.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Xml.Serialization;
-
-namespace Myvas.AspNetCore.Weixin
+﻿namespace Myvas.AspNetCore.Weixin
 {
     /// <summary>
     /// 收到文本消息
     /// </summary>
-    [XmlRoot("xml", Namespace = "")]
     public class TextMessageReceivedXml : MessageReceivedXml
     {
         public string Content { get; set; }
