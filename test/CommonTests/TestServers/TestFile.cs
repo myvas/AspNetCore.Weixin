13cd9a7cf8faeeb5e40a68fe41b5282b8c4a9fbc
FrankH <4848285@qq.com>
2025-03-13 16:23:42 +0800

    Add TestFile utility class for handling test file operations

---
+++ test/CommonTests/TestServers/TestFile.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace Myvas.AspNetCore.Weixin.Api.Tests.TestServers;
+
+public static class TestFile
+{
+    public static string GetTestFilePath(string fileName)
+    {
+        var currentDir = Directory.GetCurrentDirectory();
+        return Path.Combine(currentDir, "Data", fileName);
+    }
+
+    public static string ReadTestFile(string fileName)
+    {
+        var path = GetTestFilePath(fileName);
+        return File.ReadAllText(path);
+    }
+}
