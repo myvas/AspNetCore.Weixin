ec955c0912a01781175e267eb9d6dc7fcc54d661
FrankH <4848285@qq.com>
2025-03-13 22:56:59 +0800

    Refactor ImageMessageReceivedXml class by removing redundant code and improving structure

--- src/Core/Abstractions/WeixinXmls/ReceivedXmls/Messages/ImageMessageReceivedXml.cs
+++ src/Core/Abstractions/WeixinXmls/ReceivedXmls/Messages/ImageMessageReceivedXml.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Xml.Serialization;
+﻿using System.Xml.Serialization;
 
-namespace Myvas.AspNetCore.Weixin
+namespace Myvas.AspNetCore.Weixin;
+
+/// <summary>
+/// 收到图片消息
+/// </summary>
+[XmlRoot("xml", Namespace = "")]
+public class ImageMessageReceivedXml : MessageReceivedXml
 {
-    /// <summary>
-    /// 收到图片消息
-    /// </summary>
-    [XmlRoot("xml", Namespace = "")]
-    public class ImageMessageReceivedXml : MessageReceivedXml
-    {
-        public string PicUrl { get; set; }
-        public string MediaId { get; set; }
+    public string PicUrl { get; set; }
+    public string MediaId { get; set; }
 
-    }
 }
