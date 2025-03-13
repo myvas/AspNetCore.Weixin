6e501c00ce1e468b82bbdb67a502ec21b8f1153c
FrankH <4848285@qq.com>
2025-03-13 13:26:48 +0800

    Refactor ReceivedXml class: move to WeixinXmls folder and inherit from WeixinXml

---
+++ src/Core/Abstractions/WeixinXmls/ReceivedXmls/ReceivedXml.cs
@@ -0,0 +1,33 @@
+﻿using System.Xml.Serialization;
+
+namespace Myvas.AspNetCore.Weixin;
+
+/// <summary>
+/// 收到的微信消息或事件
+/// </summary>
+public class ReceivedXml : WeixinXml
+{
+	/// <summary>
+	/// 开发者微信号
+	/// </summary>
+	[XmlElement("ToUserName")]
+	public string ToUserName { get; set; }
+
+	/// <summary>
+	/// 发送方帐号（一个OpenID）
+	/// </summary>
+	[XmlElement("FromUserName")]
+	public string FromUserName { get; set; }
+
+	/// <summary>
+	/// 消息创建时间
+	/// </summary>
+	[XmlElement("CreateTime")]
+	public long CreateTime { get; set; }
+
+	/// <summary>
+	/// 消息类型
+	/// </summary>
+	[XmlElement("MsgType")]
+	public string MsgType { get; set; }
+}
