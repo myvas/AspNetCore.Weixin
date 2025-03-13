34b1a81d4366af5d31fd60fdbb804e682be4c9f9
FrankH <4848285@qq.com>
2025-03-13 23:58:14 +0800

    Clean up UnsubscribeEventReceivedXml by removing unnecessary usings and improving XML documentation

--- src/Core/Abstractions/WeixinXmls/ReceivedXmls/Events/UnsubscribeEventReceivedXml.cs
+++ src/Core/Abstractions/WeixinXmls/ReceivedXmls/Events/UnsubscribeEventReceivedXml.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Xml.Serialization;
+﻿using System.Xml.Serialization;
 
-namespace Myvas.AspNetCore.Weixin
+namespace Myvas.AspNetCore.Weixin;
+
+/// <summary>
+/// 退订事件
+/// </summary>
+[XmlRoot("xml", Namespace = "")]
+public class UnsubscribeEventReceivedXml : EventReceivedXml
 {
-    /// <summary>
-    /// 退订事件
-    /// </summary>
-    [XmlRoot("xml", Namespace = "")]
-    public class UnsubscribeEventReceivedXml : EventReceivedXml
-    {
-    }
 }
