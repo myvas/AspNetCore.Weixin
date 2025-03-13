9ba55f3bbfba3d5a82929069b33de2bd1732770c
FrankH <4848285@qq.com>
2025-03-13 23:57:26 +0800

    Refactor LocationEventReceivedXml by removing unnecessary usings and improving XML documentation

--- src/Core/Abstractions/WeixinXmls/ReceivedXmls/Events/LocationEventReceivedXml.cs
+++ src/Core/Abstractions/WeixinXmls/ReceivedXmls/Events/LocationEventReceivedXml.cs
@@ -1,33 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Xml.Serialization;
+﻿using System.Xml.Serialization;
 
-namespace Myvas.AspNetCore.Weixin
+namespace Myvas.AspNetCore.Weixin;
+
+/// <summary>
+/// 上报地理位置事件
+/// </summary>
+[XmlRoot("xml", Namespace = "")]
+public class LocationEventReceivedXml : EventReceivedXml
 {
     /// <summary>
-    /// 上报地理位置事件
+    /// 纬度
+    /// <example>23.134521</example>
     /// </summary>
-    [XmlRoot("xml", Namespace = "")]
-    public class LocationEventReceivedXml : EventReceivedXml
-    {
-        /// <summary>
-        /// 纬度
-        /// <example>23.134521</example>
-        /// </summary>
-        public decimal Latitude { get; set; }
+    public decimal Latitude { get; set; }
 
-        /// <summary>
-        /// 经度
-        /// <example>113.358803</example>
-        /// </summary>
-        public decimal Longitude { get; set; }
+    /// <summary>
+    /// 经度
+    /// <example>113.358803</example>
+    /// </summary>
+    public decimal Longitude { get; set; }
 
-        /// <summary>
-        /// 地理位置精度
-        /// <example>40.000000</example>
-        /// </summary>
-        public decimal Precision { get; set; }
-    }
+    /// <summary>
+    /// 地理位置精度
+    /// <example>40.000000</example>
+    /// </summary>
+    public decimal Precision { get; set; }
 }
