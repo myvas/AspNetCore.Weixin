87d3e35ef1024f7fc92ca48289f3daab0ef7f645
FrankH <4848285@qq.com>
2025-03-14 15:58:00 +0800

    Add event handling for various message types in FakeServerBuilder

--- test/WeixinSite.Tests/TestServers/FakeServerBuilder.cs
+++ test/WeixinSite.Tests/TestServers/FakeServerBuilder.cs
@@ -216,10 +216,88 @@ public static class FakeServerBuilder
             {
                 o.WebsiteToken = "WEIXINSITETOKEN";
                 o.Debug = true; // Important!
+ 
+                o.Events.OnImageMessageReceived = async (x) =>
+                {
+                    var resp = new WeixinResponseBuilder<WeixinResponseText>(x.Context, x.Xml);
+                    resp.ResponseEntity.Content = $"OnImageMessageReceived: PicUrl: {x.Xml.PicUrl}";
+                    await resp.FlushAsync();
+                    return true;
+                };
+                o.Events.OnLinkMessageReceived = async (x) =>
+                {
+                    var resp = new WeixinResponseBuilder<WeixinResponseText>(x.Context, x.Xml);
+                    resp.ResponseEntity.Content = $"OnLinkMessageReceived: Url: {x.Xml.Url}";
+                    await resp.FlushAsync();
+                    return true;
+                };
+                o.Events.OnLocationMessageReceived = async (x) =>
+                {
+                    var resp = new WeixinResponseBuilder<WeixinResponseText>(x.Context, x.Xml);
+                    resp.ResponseEntity.Content = $"OnLocationMessageReceived: Longitude: {x.Xml.Longitude} Latitude: {x.Xml.Latitude} Label: {x.Xml.Label}";
+                    await resp.FlushAsync();
+                    return true;
+                };
+                o.Events.OnShortVideoMessageReceived = async (x) =>
+                {
+                    var resp = new WeixinResponseBuilder<WeixinResponseText>(x.Context, x.Xml);
+                    resp.ResponseEntity.Content = $"OnShortVideoMessageReceived: MediaId: {x.Xml.MediaId} ThumbMediaId: {x.Xml.ThumbMediaId}";
+                    await resp.FlushAsync();
+                    return true;
+                };
                 o.Events.OnTextMessageReceived = async (x) =>
                 {
                     var resp = new WeixinResponseBuilder<WeixinResponseText>(x.Context, x.Xml);
-                    resp.ResponseEntity.Content = $"Your message had been received:{x.Xml.Content}";
+                    resp.ResponseEntity.Content = $"OnTextMessageReceived: Content: {x.Xml.Content}";
+                    await resp.FlushAsync();
+                    return true;
+                };
+                o.Events.OnVideoMessageReceived = async (x) =>
+                {
+                    var resp = new WeixinResponseBuilder<WeixinResponseText>(x.Context, x.Xml);
+                    resp.ResponseEntity.Content = $"OnVideoMessageReceived: MediaId: {x.Xml.MediaId} ThumbMediaId: {x.Xml.ThumbMediaId}";
+                    await resp.FlushAsync();
+                    return true;
+                };
+                o.Events.OnVoiceMessageReceived = async (x) =>
+                {
+                    var resp = new WeixinResponseBuilder<WeixinResponseText>(x.Context, x.Xml);
+                    resp.ResponseEntity.Content = $"OnVoiceMessageReceived: Format: {x.Xml.Format} MediaId: {x.Xml.MediaId} Recognition: {x.Xml.Recognition}";
+                    await resp.FlushAsync();
+                    return true;
+                };
+                o.Events.OnLocationEventReceived = async (x) =>
+                {
+                    var resp = new WeixinResponseBuilder<WeixinResponseText>(x.Context, x.Xml);
+                    resp.ResponseEntity.Content = $"OnLocationEventReceived: Longitude: {x.Xml.Longitude} Latitude: {x.Xml.Latitude} Precision: {x.Xml.Precision}";
+                    await resp.FlushAsync();
+                    return true;
+                };
+                o.Events.OnClickMenuEventReceived = async (x) =>
+                {
+                    var resp = new WeixinResponseBuilder<WeixinResponseText>(x.Context, x.Xml);
+                    resp.ResponseEntity.Content = $"OnClickMenuEventReceived: EventKey: {x.Xml.EventKey}";
+                    await resp.FlushAsync();
+                    return true;
+                };
+                o.Events.OnViewMenuEventReceived = async (x) =>
+                {
+                    var resp = new WeixinResponseBuilder<WeixinResponseText>(x.Context, x.Xml);
+                    resp.ResponseEntity.Content = $"OnViewMenuEventReceived: EventKey: {x.Xml.EventKey}";
+                    await resp.FlushAsync();
+                    return true;
+                };
+                o.Events.OnQrscanEventReceived = async (x) =>
+                {
+                    var resp = new WeixinResponseBuilder<WeixinResponseText>(x.Context, x.Xml);
+                    resp.ResponseEntity.Content = $"OnQrscanEventReceived: EventKey: {x.Xml.EventKey} Ticket: {x.Xml.Ticket}";
+                    await resp.FlushAsync();
+                    return true;
+                };
+                o.Events.OnSubscribeEventReceived = async (x) =>
+                {
+                    var resp = new WeixinResponseBuilder<WeixinResponseText>(x.Context, x.Xml);
+                    resp.ResponseEntity.Content = $"OnSubscribeEventReceived: EventKey: {x.Xml.EventKey} Ticket: {x.Xml.Ticket}";
                     await resp.FlushAsync();
                     return true;
                 };
