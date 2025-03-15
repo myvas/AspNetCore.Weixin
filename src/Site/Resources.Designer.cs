4dedec5b7383f56bd0f3bb8d8236e975335cf1e3
FrankH <4848285@qq.com>
2025-03-15 22:22:45 +0800

    Add localized error messages for Weixin request handling

--- src/Site/Resources.Designer.cs
+++ src/Site/Resources.Designer.cs
@@ -8,9 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Myvas.AspNetCore.Weixin {
+namespace Myvas.AspNetCore.Weixin
+{
     using System;
-        
+
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -21,86 +22,159 @@ namespace Myvas.AspNetCore.Weixin {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
-        
+    internal class Resources
+    {
+
         private static global::System.Resources.ResourceManager resourceMan;
-        
+
         private static global::System.Globalization.CultureInfo resourceCulture;
-        
+
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources() {
+        internal Resources()
+        {
         }
-        
+
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
-            get {
-                if (object.ReferenceEquals(resourceMan, null)) {
+        internal static global::System.Resources.ResourceManager ResourceManager
+        {
+            get
+            {
+                if (object.ReferenceEquals(resourceMan, null))
+                {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Myvas.AspNetCore.Weixin.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-        
+
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
-            get {
+        internal static global::System.Globalization.CultureInfo Culture
+        {
+            get
+            {
                 return resourceCulture;
             }
-            set {
+            set
+            {
                 resourceCulture = value;
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Failed on WeChat message signature verification!.
         /// </summary>
-        internal static string InvalidSignatureDenied {
-            get {
+        internal static string InvalidSignatureDenied
+        {
+            get
+            {
                 return ResourceManager.GetString("InvalidSignatureDenied", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Please access this page via the WeChat client (Official name: Micromessenger)!.
         /// </summary>
-        internal static string NonMicroMessengerDenied {
-            get {
+        internal static string NonMicroMessengerDenied
+        {
+            get
+            {
                 return ResourceManager.GetString("NonMicroMessengerDenied", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to AddEntityFrameworkStores can only be called with a Weixin subscriber that derives from WeixinSubscriber..
         /// </summary>
-        internal static string NotWeixinSubscriber {
-            get {
+        internal static string NotWeixinSubscriber
+        {
+            get
+            {
                 return ResourceManager.GetString("NotWeixinSubscriber", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to You are now trying to visit a verification URL for a WeChat Official Account server.  You can fill this URL in the “Development/Basic Configuration/Server Configuration/Server Address (URL)” field in the WeChat Official Account backend (https://mp.weixin.qq.com)..
         /// </summary>
-        internal static string WeixinSiteVerificationPathNotice {
-            get {
+        internal static string WeixinSiteVerificationPathNotice
+        {
+            get
+            {
                 return ResourceManager.GetString("WeixinSiteVerificationPathNotice", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   The weixin request content is too large.
+        /// </summary>
+        internal static string TooLargeWeixinRequestContent
+        {
+            get
+            {
+                return ResourceManager.GetString("TooLargeWeixinRequestContent", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        ///   The weixin request content cannot be parsed.
+        /// </summary>
+        internal static string BadRequest
+        {
+            get
+            {
+                return ResourceManager.GetString("BadRequest", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        /// The system is unable to recognize and process this event message.
+        /// </summary>
+        internal static string UnknownRequestMessageEvent
+        {
+            get
+            {
+                return ResourceManager.GetString("UnknownRequestMessageEvent", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        /// The system is unable to recognize and process the message.
+        /// </summary>
+        internal static string UnknownRequstMessage
+        {
+            get
+            {
+                return ResourceManager.GetString("UnknownRequestMessage", resourceCulture);
+            }
+        }
+
+        /// <summary>
+        /// An exception occurred while the system was parsing and processing the WeChat message.
+        /// </summary>
+        internal static string ExceptionOnHandler
+        {
+            get
+            {
+                return ResourceManager.GetString("ExceptionOnHandler", resourceCulture);
             }
         }
         
         /// <summary>
-        ///   The weixin request content is too large.
+        /// An error occurred on the server when calling a handler. Please contact the system adminator.
         /// </summary>
-        internal static string TooLargeWeixinRequestContent {
-            get {
-                return ResourceManager.GetString("TooLargeWeixinRequestContent", resourceCulture);
+        internal static string ErrorOnCallingHandler
+        {
+            get
+            {
+                return ResourceManager.GetString("ErrorOnCallingHandler", resourceCulture);
             }
         }
     }
