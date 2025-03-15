5f8445637649c16c4d1f828da5b87de5e3b331c8
FrankH <4848285@qq.com>
2025-03-16 00:09:01 +0800

    Add MessageReceivedEntry class to represent received non-event messages with message ID

---
+++ src/Core/Abstractions/Entities/ReceivedEntries/MessageReceivedEntry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Myvas.AspNetCore.Weixin.Models;
+
+/// <summary>
+/// 收到普通消息（除事件外的消息）
+/// </summary>
+[XmlRoot("xml", Namespace = "")]
+public class MessageReceivedEntry : ReceivedEntry
+{
+    /// <summary>
+    /// 消息id，64位整型
+    /// </summary>
+    public Int64 MsgId { get; set; }
+}
