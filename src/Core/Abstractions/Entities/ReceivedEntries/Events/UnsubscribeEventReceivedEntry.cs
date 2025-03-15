332ba3080344f3e1f15b05546ba734529308e39c
FrankH <4848285@qq.com>
2025-03-16 00:11:09 +0800

    Add UnsubscribeEventReceivedEntry class to represent unsubscribe event messages

---
+++ src/Core/Abstractions/Entities/ReceivedEntries/Events/UnsubscribeEventReceivedEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Myvas.AspNetCore.Weixin.Models;
+
+/// <summary>
+/// 退订事件
+/// </summary>
+[XmlRoot("xml", Namespace = "")]
+public class UnsubscribeEventReceivedEntry : EventReceivedEntry
+{
+}
