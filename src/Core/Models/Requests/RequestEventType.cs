73db4cb22bb30cbe3448742c6d013cbc782b9193
FrankH <4848285@qq.com>
2025-03-16 00:06:54 +0800

    Refactor RequestEventType enum to improve documentation and formatting

--- src/Core/Models/Requests/RequestEventType.cs
+++ src/Core/Models/Requests/RequestEventType.cs
@@ -1,46 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿namespace Myvas.AspNetCore.Weixin;
 
-namespace Myvas.AspNetCore.Weixin
+/// <summary>
+/// 当MsgType字段值为<see cref="RequestMsgType.@event"/>时，Event字段值即为事件类型(<see cref="RequestEventType"/>)
+/// </summary>
+/// <remarks>存储时建议使用.ToString()，而非数值。解析时应忽略大小写。</remarks>
+public enum RequestEventType
 {
     /// <summary>
-    /// 当MsgType字段值为<see cref="RequestMsgType.@event"/>时，Event字段值即为事件类型(<see cref="RequestEventType"/>)
+    /// 未知
     /// </summary>
-    /// <remarks>存储时建议使用.ToString()，而非数值。解析时应忽略大小写。</remarks>
-    public enum RequestEventType
-    {
-        Unknown = 0,
+    Unknown = 0,
 
-        /// <summary>
-        /// 1.1 订阅
-        /// </summary>
-        subscribe = 100,
+    /// <summary>
+    /// 1.1 订阅
+    /// </summary>
+    subscribe = 100,
 
-		/// <summary>
-		/// 1.2 取消订阅
-		/// </summary>
-		unsubscribe = 200,
+    /// <summary>
+    /// 1.2 取消订阅
+    /// </summary>
+    unsubscribe = 200,
 
-        /// <summary>
-        /// 300 二维码扫描
-        /// </summary>
-        SCAN = 300,
+    /// <summary>
+    /// 300 二维码扫描
+    /// </summary>
+    SCAN = 300,
 
-        /// <summary>
-        /// 400 上报地理位置事件，注意区别于“地理位置上行消息”
-        /// </summary>
-        LOCATION = 400,
+    /// <summary>
+    /// 400 上报地理位置事件，注意区别于“地理位置上行消息”
+    /// </summary>
+    LOCATION = 400,
 
-        /// <summary>
-        /// 500 自定义菜单事件：拉取消息
-        /// </summary>
-        CLICK = 500,
+    /// <summary>
+    /// 500 自定义菜单事件：拉取消息
+    /// </summary>
+    CLICK = 500,
 
-        /// <summary>
-        /// 600 自定义菜单事件：跳转链接
-        /// </summary>
-        VIEW = 600
-    }
+    /// <summary>
+    /// 600 自定义菜单事件：跳转链接
+    /// </summary>
+    VIEW = 600
 }
