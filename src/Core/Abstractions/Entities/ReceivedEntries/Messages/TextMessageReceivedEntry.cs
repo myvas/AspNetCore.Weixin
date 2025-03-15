359f664155f63035d569d6578eaad0230a93af31
FrankH <4848285@qq.com>
2025-03-16 00:13:14 +0800

    Add TextMessageReceivedEntry class to represent received text messages with Content property

---
+++ src/Core/Abstractions/Entities/ReceivedEntries/Messages/TextMessageReceivedEntry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Myvas.AspNetCore.Weixin.Models;
+
+/// <summary>
+/// 收到文本消息
+/// </summary>
+[XmlRoot("xml", Namespace = "")]
+public class TextMessageReceivedEntry : MessageReceivedEntry
+{
+    /// <summary>
+    /// 文本消息内容
+    /// </summary>
+    public string Content { get; set; }
+}
