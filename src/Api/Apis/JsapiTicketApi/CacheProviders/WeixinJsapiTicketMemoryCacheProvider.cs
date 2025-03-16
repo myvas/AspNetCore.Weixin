1eac42fd895244dab8c7c8d447658c9e4b78b7cf
FrankH <4848285@qq.com>
2025-03-16 20:52:23 +0800

    Refactor WeixinJsapiTicketMemoryCacheProvider to inherit from WeixinExpirationMemoryCacheProvider for improved cache expiration handling

--- src/Api/Apis/JsapiTicketApi/CacheProviders/WeixinJsapiTicketMemoryCacheProvider.cs
+++ src/Api/Apis/JsapiTicketApi/CacheProviders/WeixinJsapiTicketMemoryCacheProvider.cs
@@ -5,7 +5,7 @@ namespace Myvas.AspNetCore.Weixin;
 /// <summary>
 /// Manage their cached <see cref="WeixinJsapiTicketJson"> for the Weixin accounts specified by 'appId'
 /// </summary>
-public class WeixinJsapiTicketMemoryCacheProvider : WeixinMemoryCacheProvider<WeixinJsapiTicketJson>, IWeixinJsapiTicketCacheProvider
+public class WeixinJsapiTicketMemoryCacheProvider : WeixinExpirationMemoryCacheProvider<WeixinJsapiTicketJson>, IWeixinJsapiTicketCacheProvider
 {
     public WeixinJsapiTicketMemoryCacheProvider(IMemoryCache cache) : base(cache)
     {
