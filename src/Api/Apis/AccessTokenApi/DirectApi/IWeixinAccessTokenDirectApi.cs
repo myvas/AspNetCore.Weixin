bf5a324cbc926908d05396d49e9d619f20c2c68f
FrankH <4848285@qq.com>
2025-03-16 13:26:49 +0800

    Enhance IWeixinAccessTokenDirectApi with additional refresh token methods and improve documentation clarity

--- src/Api/Apis/AccessTokenApi/DirectApi/IWeixinAccessTokenDirectApi.cs
+++ src/Api/Apis/AccessTokenApi/DirectApi/IWeixinAccessTokenDirectApi.cs
@@ -4,22 +4,34 @@ using System.Threading.Tasks;
 namespace Myvas.AspNetCore.Weixin;
 
 /// <summary>
-/// WARNING! <see cref="IWeixinAccessTokenDirectApi"/> will directly send a request to the Tencent Server to fetch a new access token each time!
+/// WARNING! This api will directly send a request to the Tencent Server.
 /// </summary>
 /// <remarks>
-// Notice: We have removed IWeixinAccessTokenDirectApi from Dependency Injection (DI) because it fetches a new access token each time it is called.
-// You should conside to use <see also="IWeixinAccessTokenApi"/>, and enable a cache provider for your access token.</remarks>
+/// Notice: You should conside to use <see also="IWeixinAccessTokenApi"/>, and enable a cache provider for your access token.
+/// </remarks>
 public interface IWeixinAccessTokenDirectApi
 {
     /// <summary>
-    /// 获取新的微信API访问凭证。
+    /// 获取微信公众号全局接口调用凭证(access_token)。
     /// </summary>
-    /// <returns>微信API访问凭证</returns>
+    /// <returns>微信公众号全局接口调用凭证(access_token)</returns>
     Task<WeixinAccessTokenJson> GetTokenAsync(CancellationToken cancellationToken = default);
 
     /// <summary>
-    /// 获取新的微信API访问凭证。
+    /// 获取微信公众号全局接口调用凭证(access_token)。
     /// </summary>
-    /// <returns>微信API访问凭证</returns>
+    /// <returns>微信公众号全局接口调用凭证(access_token)</returns>
     WeixinAccessTokenJson GetToken();
+
+    /// <summary>
+    /// 强制刷新微信公众号全局接口调用凭证(access_token)。
+    /// </summary>
+    /// <returns>微信公众号全局接口调用凭证(access_token)</returns>
+    Task<WeixinAccessTokenJson> RefreshTokenAsync(CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// 强制刷新微信公众号全局接口调用凭证(access_token)。
+    /// </summary>
+    /// <returns>微信公众号全局接口调用凭证(access_token)</returns>
+    WeixinAccessTokenJson RefreshToken();
 }
