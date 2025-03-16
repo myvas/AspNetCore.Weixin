0fd6438e220b5c770555246e71ba26593580ecdf
FrankH <4848285@qq.com>
2025-03-16 20:46:50 +0800

    Rename cache extension methods for JSON serialization and deserialization for clarity

--- src/Api/Abstrations/CacheProviders/IDistributedCacheJsonExtensions.cs
+++ src/Api/Abstrations/CacheProviders/IDistributedCacheJsonExtensions.cs
@@ -6,16 +6,13 @@ using Microsoft.Extensions.Caching.Distributed;
 
 public static class IDistributedCacheGenericExtensions
 {
-    public static Task SetAsync<T>(this IDistributedCache cache, string key, T value, CancellationToken cancellationToken = default)
-        => SetAsync(cache, key, value, new DistributedCacheEntryOptions());
-    
-    public static Task SetAsync<T>(this IDistributedCache cache, string key, T value, DistributedCacheEntryOptions options, CancellationToken cancellationToken = default)
+    public static Task SetAsJsonAsync<T>(this IDistributedCache cache, string key, T value, DistributedCacheEntryOptions options, CancellationToken cancellationToken = default)
     {
         var bytes = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(value, GetJsonSerializerOptions()));
         return cache.SetAsync(key, bytes, options);
     }
 
-    public static async Task<T> GetAsync<T>(this IDistributedCache cache, string key, CancellationToken cancellationToken = default)
+    public static async Task<T> GetFromJsonAsync<T>(this IDistributedCache cache, string key, CancellationToken cancellationToken = default)
     {
         var bytes = await cache.GetAsync(key);
         return (bytes == null) ? default : JsonSerializer.Deserialize<T>(bytes, GetJsonSerializerOptions());
