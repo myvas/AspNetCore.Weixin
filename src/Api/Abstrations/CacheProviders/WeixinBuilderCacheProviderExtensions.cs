3da5ef827d2c54d44f2a70be36fae6525c68a846
FrankH <4848285@qq.com>
2025-03-16 20:48:18 +0800

    Refactor Weixin cache provider extensions to use IWeixinExpirableValue for improved cache handling

--- src/Api/Abstrations/CacheProviders/WeixinBuilderCacheProviderExtensions.cs
+++ src/Api/Abstrations/CacheProviders/WeixinBuilderCacheProviderExtensions.cs
@@ -7,27 +7,27 @@ namespace Microsoft.Extensions.DependencyInjection;
 public static class WeixinBuilderCacheProviderExtensions
 {
     public static WeixinBuilder AddMemoryCacheProvider<TWeixinCacheJson>(this WeixinBuilder builder)
-        where TWeixinCacheJson : IWeixinCacheJson
+        where TWeixinCacheJson : IWeixinExpirableValue, new()
     {
         builder.Services.AddMemoryCache();
         builder.Services.Where(x => x.ImplementationType == typeof(IWeixinCacheProvider<TWeixinCacheJson>)).ToList()
             .ForEach(x => builder.Services.Remove(x));
-        builder.Services.AddSingleton<IWeixinCacheProvider<TWeixinCacheJson>, WeixinMemoryCacheProvider<TWeixinCacheJson>>();
+        builder.Services.AddSingleton<IWeixinCacheProvider<TWeixinCacheJson>, WeixinExpirationMemoryCacheProvider<TWeixinCacheJson>>();
         return builder;
     }
 
     public static WeixinBuilder AddRedisCacheProvider<TWeixinCacheJson>(this WeixinBuilder builder)
-        where TWeixinCacheJson : IWeixinCacheJson
+        where TWeixinCacheJson : IWeixinExpirableValue, new()
     {
         builder.Services.AddMemoryCache();
         builder.Services.Where(x => x.ImplementationType == typeof(IWeixinCacheProvider<TWeixinCacheJson>)).ToList()
             .ForEach(x => builder.Services.Remove(x));
-        builder.Services.AddSingleton<IWeixinCacheProvider<TWeixinCacheJson>, WeixinRedisCacheProvider<TWeixinCacheJson>>();
+        builder.Services.AddSingleton<IWeixinCacheProvider<TWeixinCacheJson>, WeixinExpirationRedisCacheProvider<TWeixinCacheJson>>();
         return builder;
     }
 
-    public static WeixinBuilder AddCacheProvider<TWeixinCacheJson, TWeixinCacheProvider>(this WeixinBuilder builder)
-        where TWeixinCacheJson : IWeixinCacheJson
+    public static WeixinBuilder AddWeixinCacheProvider<TWeixinCacheJson, TWeixinCacheProvider>(this WeixinBuilder builder)
+        where TWeixinCacheJson : IWeixinExpirableValue
         where TWeixinCacheProvider : class, IWeixinCacheProvider<TWeixinCacheJson>
     {
         builder.Services.Where(x => x.ImplementationType == typeof(IWeixinCacheProvider<TWeixinCacheJson>)).ToList()
