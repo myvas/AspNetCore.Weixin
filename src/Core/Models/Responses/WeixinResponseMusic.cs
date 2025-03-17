b4ca466fd4359895055c243c22480e3240678310
FrankH <4848285@qq.com>
2025-03-17 14:41:23 +0800

    Refactor WeixinResponseMusic class to simplify constructor and enhance ToXml method

--- src/Core/Models/Responses/WeixinResponseMusic.cs
+++ src/Core/Models/Responses/WeixinResponseMusic.cs
@@ -1,44 +1,40 @@
 ﻿using System.Xml.Serialization;
 
-namespace Myvas.AspNetCore.Weixin
+namespace Myvas.AspNetCore.Weixin;
+
+[XmlRoot("xml", Namespace = "")]
+public class WeixinResponseMusic : WeixinResponse, IWeixinResponse
 {
-    [XmlRoot("xml", Namespace = "")]
-    public class WeixinResponseMusic : WeixinResponse, IWeixinResponse
+    public WeixinResponseMusic()
     {
-        public WeixinResponseMusic()
+        MsgType = ResponseMsgType.music;
+    }
+
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string MusicUrl { get; set; }
+    [XmlElement("HQMusicUrl")]
+    public string HQMusicUrl { get; set; }
+    public string ThumbMediaId { get; set; }
+
+
+    public override string ToXml()
+    {
+        var data = new
         {
-            MsgType = ResponseMsgType.music;
-        }
-
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string MusicUrl { get; set; }
-        [XmlElement("HQMusicUrl")]
-        public string HQMusicUrl { get; set; }
-        public string ThumbMediaId { get; set; }
-
-
-        public override string ToXml()
-        {
-            var data = new
+            ToUserName,
+            FromUserName,
+            CreateTime = CreateTimestamp,
+            MsgType = MsgTypeText,
+            MUSIC = new
             {
-                xml = new
-                {
-                    ToUserName,
-                    FromUserName,
-                    CreateTime = CreateTimestamp,
-                    MsgType = MsgTypeText,
-                    MUSIC = new
-                    {
-                        Title,
-                        Description,
-                        MusicUrl,
-                        HQMusicUrl,
-                        ThumbMediaId
-                    }
-                }
-            };
-            return MyvasXmlConvert.SerializeObject(data);
-        }
+                Title,
+                Description,
+                MusicUrl,
+                HQMusicUrl,
+                ThumbMediaId
+            }
+        };
+        return WeixinXmlConvert.SerializeObject(data);
     }
 }
