eb9fb833fb23b219cc312e0e72e422537c8ac55d
FrankH <4848285@qq.com>
2025-03-17 16:41:51 +0800

    Refactor WeixinResponseBuilder to WeixinResponseMessageBuilder for improved message handling

---
+++ src/Site/Responses/WeixinResponseMessageBuilder.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace Myvas.AspNetCore.Weixin;
+
+/// <summary>
+/// Create a 
+/// </summary>
+/// <typeparam name="TWeixinResponseMessage"></typeparam>
+public class WeixinResponseMessageBuilder<TWeixinResponseMessage> : WeixinResponseXmlBuilder
+    where TWeixinResponseMessage : IWeixinResponseMessage, new()
+{
+    public WeixinResponseMessageBuilder(HttpContext context, ReceivedXml receivedXml) : base(context)
+    {
+        ReceivedXml = receivedXml;
+        ResponseMessage = new TWeixinResponseMessage();
+        ResponseMessage.FromUserName = receivedXml.ToUserName;
+        ResponseMessage.ToUserName = receivedXml.FromUserName;
+        ResponseMessage.CreateTime = DateTime.Now;
+    }
+
+    public ReceivedXml ReceivedXml { get; private set; }
+    public TWeixinResponseMessage ResponseMessage { get; private set; }
+
+    public override Task FlushAsync()
+    {
+        Content = ResponseMessage.ToXml();
+        return base.FlushAsync();
+    }
+}
