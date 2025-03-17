5a891ca79d5f217d8291d3e7bd4854251e3798eb
FrankH <4848285@qq.com>
2025-03-17 17:06:21 +0800

    Add WeixinSiteMessageProtectionOptions for message protection configuration

--- src/Site/Providers/WeixinSiteMessageProtectionOptions.cs
+++ src/Site/Providers/WeixinSiteMessageProtectionOptions.cs
@@ -4,7 +4,7 @@
 /// Options for the WeixinSite encoding provider <see cref="WeixinMessageEncryptor"/>.
 /// </summary>
 /// <remarks>This options is created in <see cref="WeixinSitePostConfigureOptions"/>.</remarks>
-public class WeixinSiteEncodingOptions
+public class WeixinSiteMessageProtectionOptions
 {
     /// <summary>
     /// The AppId comes from <see cref="WeixinOptions"/>.
@@ -23,7 +23,12 @@ public class WeixinSiteEncodingOptions
 
     /// <summary>
     /// 采用严格模式，即：所有消息必须加密，不允许明文消息
-    /// <para>default is <see cref="WeixinSiteOptionsDefaults.StrictMode"/>  which is false</para>
+    /// <para>default is <see cref="WeixinSiteOptionsDefaults.StrictMode"/> which is false</para>
     /// </summary>
+    /// <remarks>
+    /// - 兼容模式: 同时支持明文消息和加密消息。
+    /// - 明文模式，只支持明文消息，不加密消息。
+    /// - 加密模式，只支持加密模式，不支持明文消息。
+    /// </remarks>
     public bool StrictMode { get; set; } = WeixinSiteOptionsDefaults.StrictMode;
 }
