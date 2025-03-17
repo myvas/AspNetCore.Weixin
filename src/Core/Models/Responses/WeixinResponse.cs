0441649cb2c7de99161efdabd2272ec76981cb69
FrankH <4848285@qq.com>
2025-03-17 14:40:45 +0800

    Refactor WeixinResponse class to improve MsgType handling and update serialization method

--- src/Core/Models/Responses/WeixinResponse.cs
+++ src/Core/Models/Responses/WeixinResponse.cs
@@ -1,32 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Xml.Linq;
 using System.Xml.Serialization;
 
-namespace Myvas.AspNetCore.Weixin
+namespace Myvas.AspNetCore.Weixin;
+
+/// <summary>
+/// 响应回复消息
+/// </summary>
+[XmlRoot("xml", Namespace = "")]
+public abstract class WeixinResponse : WeixinMessage, IWeixinResponse
 {
+    [XmlElement("MsgType", Namespace = "")]
+    public string MsgTypeText { get; set; }
 
-    /// <summary>
-    /// 响应回复消息
-    /// </summary>
-    [XmlRoot("xml", Namespace = "")]
-    public abstract class WeixinResponse : WeixinMessage, IWeixinResponse
+    [XmlIgnore]
+    public ResponseMsgType MsgType
     {
-        [XmlElement("MsgType", Namespace = "")]
-        public string MsgTypeText { get; set; }
+        get => (ResponseMsgType)Enum.Parse(typeof(ResponseMsgType), MsgTypeText);
+        set => MsgTypeText = value.ToString();
+    }
 
-        [XmlIgnore]
-        public ResponseMsgType MsgType
-        {
-            get => (ResponseMsgType)Enum.Parse(typeof(ResponseMsgType), MsgTypeText);
-            set => MsgTypeText = value.ToString();
-        }
-
-        public virtual string ToXml()
-        {
-            return MyvasXmlConvert.SerializeObject(this);
-        }
+    public virtual string ToXml()
+    {
+        return WeixinXmlConvert.SerializeObject(this);
     }
 }
