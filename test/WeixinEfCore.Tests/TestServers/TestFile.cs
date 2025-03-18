eac68257aa07cd0dc40f5100ce3a49d8c73b7227
FrankH <4848285@qq.com>
2025-03-18 20:39:50 +0800

    Add TestFile utility class for managing test file paths and reading content

---
+++ test/WeixinEfCore.Tests/TestServers/TestFile.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace Myvas.AspNetCore.Weixin.EfCore.Tests;
+
+public static class TestFile
+{
+    public static string GetTestFilePath(string fileName)
+    {
+        var currentDir = Directory.GetCurrentDirectory();
+        return Path.Combine(currentDir, "Data", fileName);
+    }
+    public static string ReadTestFile(string fileName)
+    {
+        var path = GetTestFilePath(fileName);
+        return File.ReadAllText(path);
+    }
+}
