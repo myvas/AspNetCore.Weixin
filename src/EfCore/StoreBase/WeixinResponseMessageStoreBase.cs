9b56a650db3d8c0647201194d46aa8bcbd1e7677
FrankH <4848285@qq.com>
2025-03-19 12:46:45 +0800

    Refactor WeixinResponseMessageStoreBase for improved error handling and code clarity

--- src/EfCore/StoreBase/WeixinResponseMessageStoreBase.cs
+++ src/EfCore/StoreBase/WeixinResponseMessageStoreBase.cs
@@ -4,48 +4,47 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Myvas.AspNetCore.Weixin
+namespace Myvas.AspNetCore.Weixin;
+
+public abstract class WeixinResponseMessageStoreBase<TWeixinResponseMessage> : EntityStoreBase<TWeixinResponseMessage>, IWeixinResponseMessageStore<TWeixinResponseMessage>
+    where TWeixinResponseMessage : WeixinResponseMessage
 {
-    public abstract class WeixinResponseMessageStoreBase<TWeixinResponseMessage> : EntityStoreBase<TWeixinResponseMessage>, IWeixinResponseMessageStore<TWeixinResponseMessage>
-        where TWeixinResponseMessage : WeixinResponseMessage
+    public WeixinResponseMessageStoreBase(WeixinErrorDescriber describer)
     {
-        public WeixinResponseMessageStoreBase(WeixinErrorDescriber describer)
-        {
-            ErrorDescriber = describer ?? throw new ArgumentNullException(nameof(describer));
-        }
+        ErrorDescriber = describer ?? throw new ArgumentNullException(nameof(describer));
+    }
 
-        /// <summary>
-        /// Gets or sets the <see cref="WeixinErrorDescriber"/> for any error that occurred with the current operation.
-        /// </summary>
-        public WeixinErrorDescriber ErrorDescriber { get; set; }
+    /// <summary>
+    /// Gets or sets the <see cref="WeixinErrorDescriber"/> for any error that occurred with the current operation.
+    /// </summary>
+    public WeixinErrorDescriber ErrorDescriber { get; set; }
 
-        public virtual Task<int> GetCountByDestOpenIdAsync(string openId, CancellationToken cancellationToken = default)
+    public virtual Task<int> GetCountByDestOpenIdAsync(string openId, CancellationToken cancellationToken = default)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+        ThrowIfDisposed();
+        if (openId == null)
         {
-            cancellationToken.ThrowIfCancellationRequested();
-            ThrowIfDisposed();
-            if (openId == null)
-            {
-                throw new ArgumentNullException(nameof(openId));
-            }
-            var result = Items.Count(x => x.ToUserName == openId);
-            return Task.FromResult(result);
+            throw new ArgumentNullException(nameof(openId));
         }
+        var result = Items.Count(x => x.ToUserName == openId);
+        return Task.FromResult(result);
+    }
 
-        public virtual Task<IList<TWeixinResponseMessage>> GetItemsByDestOpenIdAsync(string openId, int perPage, int pageIndex, CancellationToken cancellationToken = default)
+    public virtual Task<IList<TWeixinResponseMessage>> GetItemsByDestOpenIdAsync(string openId, int perPage, int pageIndex, CancellationToken cancellationToken = default)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+        ThrowIfDisposed();
+        if (openId == null)
         {
-            cancellationToken.ThrowIfCancellationRequested();
-            ThrowIfDisposed();
-            if (openId == null)
-            {
-                throw new ArgumentNullException(nameof(openId));
-            }
-            var result = Items.Where(x => x.ToUserName == openId);
-            return Task.FromResult((IList<TWeixinResponseMessage>)result);
+            throw new ArgumentNullException(nameof(openId));
         }
+        var result = Items.Where(x => x.ToUserName == openId);
+        return Task.FromResult((IList<TWeixinResponseMessage>)result);
+    }
 
-        public virtual Task<IList<TWeixinResponseMessage>> GetRequestMessagesAsync(string id, int perPage, int pageIndex, CancellationToken cancellationToken = default)
-        {
-            throw new NotImplementedException();
-        }
+    public virtual Task<IList<TWeixinResponseMessage>> GetRequestMessagesAsync(string id, int perPage, int pageIndex, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
     }
 }
