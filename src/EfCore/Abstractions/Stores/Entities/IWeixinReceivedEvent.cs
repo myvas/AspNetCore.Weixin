b5973d1f800c9c23ea61acbf8e51a1c6ec172ed4
FrankH <4848285@qq.com>
2025-03-19 12:48:32 +0800

    Refactor IWeixinReceivedEvent interface for improved structure and readability

--- src/EfCore/Abstractions/Stores/Entities/IWeixinReceivedEvent.cs
+++ src/EfCore/Abstractions/Stores/Entities/IWeixinReceivedEvent.cs
@@ -1,35 +1,34 @@
-﻿namespace Myvas.AspNetCore.Weixin
-{
-    public interface IWeixinReceivedEvent
-    {
-        string Event { get; set; }
-    }
+﻿namespace Myvas.AspNetCore.Weixin;
 
-    public interface IWeixinReceivedEventSubscribe : IWeixinReceivedEvent
-    {
-    }
-    public interface IWeixinReceivedEventSubscribeWithScene
-    {
-        string EventKey { get; set; }
-        string Ticket { get; set; }
-    }
-    public interface IWeixinReceivedEventQrscan : IWeixinReceivedEvent
-    {
-        string EventKey { get; set; }
-        string Ticket { get; set; }
-    }
-    public interface IWeixinReceivedEventLocation : IWeixinReceivedEvent
-    {
-        decimal? Longitude { get; set; }
-        decimal? Latitude { get; set; }
-        decimal? Precision { get; set; }
-    }
-    public interface IWeixinReceivedEventViewMenu : IWeixinReceivedEvent
-    {
-        string EventKey { get; set; }
-    }
-    public interface IWeixinReceivedEventClickMenu : IWeixinReceivedEvent
-    {
-        string EventKey { get; set; }
-    }
+public interface IWeixinReceivedEvent
+{
+    string Event { get; set; }
+}
+
+public interface IWeixinReceivedEventSubscribe : IWeixinReceivedEvent
+{
+}
+public interface IWeixinReceivedEventSubscribeWithScene
+{
+    string EventKey { get; set; }
+    string Ticket { get; set; }
+}
+public interface IWeixinReceivedEventQrscan : IWeixinReceivedEvent
+{
+    string EventKey { get; set; }
+    string Ticket { get; set; }
+}
+public interface IWeixinReceivedEventLocation : IWeixinReceivedEvent
+{
+    decimal? Longitude { get; set; }
+    decimal? Latitude { get; set; }
+    decimal? Precision { get; set; }
+}
+public interface IWeixinReceivedEventViewMenu : IWeixinReceivedEvent
+{
+    string EventKey { get; set; }
+}
+public interface IWeixinReceivedEventClickMenu : IWeixinReceivedEvent
+{
+    string EventKey { get; set; }
 }
