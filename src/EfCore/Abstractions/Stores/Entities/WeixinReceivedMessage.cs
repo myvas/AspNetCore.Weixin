6dfe2eac4eeb599fc93927b51450e8cc7bc1635a
FrankH <4848285@qq.com>
2025-03-19 12:47:53 +0800

    Refactor WeixinReceivedMessage class for improved structure and readability

--- src/EfCore/Abstractions/Stores/Entities/WeixinReceivedMessage.cs
+++ src/EfCore/Abstractions/Stores/Entities/WeixinReceivedMessage.cs
@@ -1,44 +1,43 @@
 ﻿using System;
 
-namespace Myvas.AspNetCore.Weixin
+namespace Myvas.AspNetCore.Weixin;
+
+public class WeixinReceivedMessage : Entity,
+    IWeixinReceivedText,
+    IWeixinReceivedImage,
+    IWeixinReceivedVoice,
+    IWeixinReceivedVoiceWithRecognition,
+    IWeixinReceivedVideo,
+    IWeixinReceivedShortVideo,
+    IWeixinReceivedLink,
+    IWeixinReceivedLocation
 {
-    public class WeixinReceivedMessage : Entity,
-        IWeixinReceivedText,
-        IWeixinReceivedImage,
-        IWeixinReceivedVoice,
-        IWeixinReceivedVoiceWithRecognition,
-        IWeixinReceivedVideo,
-        IWeixinReceivedShortVideo,
-        IWeixinReceivedLink,
-        IWeixinReceivedLocation
+    public virtual string FromUserName { get; set; }
+    public virtual string ToUserName { get; set; }
+    public virtual long CreateTime { get; set; }
+    public virtual DateTimeOffset? CreateTimeOffset
     {
-        public virtual string FromUserName { get; set; }
-        public virtual string ToUserName { get; set; }
-        public virtual long CreateTime { get; set; }
-        public virtual DateTimeOffset? CreateTimeOffset
-        {
-            get { return WeixinTimestampHelper.ToLocalTime(CreateTime); }
-        }
-        public virtual string MsgType { get; set; }
-
-        public virtual long MsgId { get; set; }
-        public virtual string Content { get; set; }
-        public virtual string PicUrl { get; set; }
-        public virtual string MediaId { get; set; }
-        public virtual string Format { get; set; }
-        public virtual string Recognition { get; set; }
-        public virtual string ThumbMediaId { get; set; }
-        public virtual decimal? Longitude { get; set; }
-        public virtual decimal? Latitude { get; set; }
-        public virtual decimal? Scale { get; set; }
-        public virtual string Label { get; set; }
-        public virtual string Title { get; set; }
-        public virtual string Url { get; set; }
-        public virtual string Description { get; set; }
-
-        /// <summary>
-        /// A random value that must change whenever a user is persisted to the store
-        /// </summary>
-        public virtual string ConcurrencyStamp { get; set; } = Guid.NewGuid().ToString();
+        get { return WeixinTimestampHelper.ToLocalTime(CreateTime); }
     }
+    public virtual string MsgType { get; set; }
+
+    public virtual long MsgId { get; set; }
+    public virtual string Content { get; set; }
+    public virtual string PicUrl { get; set; }
+    public virtual string MediaId { get; set; }
+    public virtual string Format { get; set; }
+    public virtual string Recognition { get; set; }
+    public virtual string ThumbMediaId { get; set; }
+    public virtual decimal? Longitude { get; set; }
+    public virtual decimal? Latitude { get; set; }
+    public virtual decimal? Scale { get; set; }
+    public virtual string Label { get; set; }
+    public virtual string Title { get; set; }
+    public virtual string Url { get; set; }
+    public virtual string Description { get; set; }
+
+    /// <summary>
+    /// A random value that must change whenever a user is persisted to the store
+    /// </summary>
+    public virtual string ConcurrencyStamp { get; set; } = Guid.NewGuid().ToString();
 }
