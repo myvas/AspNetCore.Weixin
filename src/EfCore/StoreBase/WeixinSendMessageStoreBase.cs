94d4463f1dcb40ffe76aeaaa94601340ef4a9708
FrankH <4848285@qq.com>
2025-03-19 12:45:54 +0800

    Refactor WeixinSendMessageStoreBase constructor for improved clarity and structure

--- src/EfCore/StoreBase/WeixinSendMessageStoreBase.cs
+++ src/EfCore/StoreBase/WeixinSendMessageStoreBase.cs
@@ -4,20 +4,19 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Myvas.AspNetCore.Weixin
+namespace Myvas.AspNetCore.Weixin;
+
+public abstract class WeixinSendMessageStoreBase<TWeixinSendMessage> : EntityStoreBase<TWeixinSendMessage>, IWeixinSendMessageStore<TWeixinSendMessage>
+    where TWeixinSendMessage : WeixinSendMessage
 {
-    public abstract class WeixinSendMessageStoreBase<TWeixinSendMessage> : EntityStoreBase<TWeixinSendMessage>, IWeixinSendMessageStore<TWeixinSendMessage>
-        where TWeixinSendMessage : WeixinSendMessage
+    public WeixinSendMessageStoreBase(WeixinErrorDescriber describer)
     {
-        public WeixinSendMessageStoreBase(WeixinErrorDescriber describer)
-        {
-            ErrorDescriber = describer ?? throw new ArgumentNullException(nameof(describer));
-        }
-
-        /// <summary>
-        /// Gets or sets the <see cref="WeixinErrorDescriber"/> for any error that occurred with the current operation.
-        /// </summary>
-        public WeixinErrorDescriber ErrorDescriber { get; set; }
-
+        ErrorDescriber = describer ?? throw new ArgumentNullException(nameof(describer));
     }
+
+    /// <summary>
+    /// Gets or sets the <see cref="WeixinErrorDescriber"/> for any error that occurred with the current operation.
+    /// </summary>
+    public WeixinErrorDescriber ErrorDescriber { get; set; }
+
 }
