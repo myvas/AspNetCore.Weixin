ea9f2c3c4eafa9d5031f20fbca5e46e166945cbd
FrankH <4848285@qq.com>
2025-03-22 15:20:14 +0800

    Add DbContext that implements IWeixinDbContext

---
+++ samples/WeixinSiteSample/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Myvas.AspNetCore.Weixin;
+
+namespace WeixinSiteSample.Data;
+
+public class AppDbContext : IdentityDbContext, IWeixinDbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected AppDbContext()
+    {
+    }
+
+    public DbSet<WeixinSubscriberEntity> WeixinSubscribers { get; set; }
+    public DbSet<WeixinReceivedEventEntity> WeixinReceivedEvents { get; set; }
+    public DbSet<WeixinReceivedMessageEntity> WeixinReceivedMessages { get; set; }
+    public DbSet<WeixinResponseMessageEntity> WeixinResponseMessages { get; set; }
+    public DbSet<WeixinSendMessageEntity> WeixinSendMessages { get; set; }
+}
