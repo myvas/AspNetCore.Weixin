2e3771b098731ff8352526af13a812905bf7c172
FrankH <4848285@qq.com>
2025-03-25 04:39:24 +0800

    Update user cases

--- test/WeixinSite.Tests/WeixinSiteMiddlewareTests/WeixinSiteMiddlewareTests.cs
+++ test/WeixinSite.Tests/WeixinSiteMiddlewareTests/WeixinSiteMiddlewareTests.cs
@@ -1,16 +1,13 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Myvas.AspNetCore.Weixin.Site.Tests.TestServers;
 using System;
 using System.Diagnostics;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Xunit;
 
 namespace Myvas.AspNetCore.Weixin.Site.Tests;
 
@@ -22,7 +19,34 @@ public class WeixinSiteMiddlewareTests
 
     public WeixinSiteMiddlewareTests()
     {
-        testServer = FakeServerBuilder.CreateTencentServer();
+        testServer = TestServerBuilder.CreateServer(app =>
+        {
+            app.UseWeixinSite();
+        }, services =>
+        {
+            services.AddWeixin(o =>
+            {
+                o.AppId = "APPID";
+                o.AppSecret = "APPSECRET";
+            })
+            .AddWeixinSite(o =>
+            {
+                o.WebsiteToken = "WEIXINSITETOKEN";
+            })
+            .AddMessageProtection();
+        }, async context =>
+        {
+            var req = context.Request;
+            if (req.Path.Value != WeixinSiteOptionsDefaults.Path)
+            {
+
+                context.Response.StatusCode = StatusCodes.Status404NotFound;
+                var content = "404 NOT FOUND";
+                await context.Response.WriteAsync(content);
+                return true;
+            }
+            return false;
+        });
     }
 
     [Fact]
