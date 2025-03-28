565e669b17c0eec1dac0abf9cd04e276079ea7eb
FrankH <4848285@qq.com>
2025-03-28 18:17:45 +0800

    Adjust IWeixinErrorJson for no exception implementations

--- src/Api/Abstrations/ResultJsons/IWeixinErrorJson.cs
+++ src/Api/Abstrations/ResultJsons/IWeixinErrorJson.cs
@@ -1,33 +1,44 @@
 ﻿using System;
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
 
 namespace Myvas.AspNetCore.Weixin;
 
-public interface IWeixinError
+public interface IWeixinErrorJson
 {
+    [XmlIgnore]
+    [JsonIgnore]
     bool Succeeded { get; }
 
     /// <summary>
     /// 微信错误代码
     /// </summary>
-    int? ErrorCode { get; }
+    int? ErrorCode { get; set; }
 
     /// <summary>
     /// 微信错误描述
     /// </summary>
-    string ErrorMessage { get; }
+    string ErrorMessage { get; set; }
+
+    [XmlIgnore]
+    [JsonIgnore]
+    Exception Exception { get; set; }
 
     #region deprecated
     /// <summary>
     /// 微信错误代码
     /// </summary>
     [Obsolete("Use ErrorCode instead.")]
+    [XmlIgnore]
+    [JsonIgnore]
     int? errcode { get; }
-
 
     /// <summary>
     /// 微信错误描述
     /// </summary>
     [Obsolete("Use ErrorMessage instead.")]
+    [XmlIgnore]
+    [JsonIgnore]
     string errmsg { get; }
     #endregion
 }
